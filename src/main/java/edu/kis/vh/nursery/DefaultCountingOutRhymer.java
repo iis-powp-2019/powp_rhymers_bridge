d27a9ad64f424f20d526f5d6aef068fd418763c1
astepaniuk <truemob2@gmail.com>
2019-03-05 11:37:11 +0100

    lab1_task_3.1.5

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int STACK_SIZE = 12;
+    public static final int EMPTY_FLAG = -1;
+    public static final int FULL_FLAG = 11;
+    private int[] numbers = new int[STACK_SIZE];
 
-    public int total = -1;
+    public int total = EMPTY_FLAG;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_FLAG;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_FLAG;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY_FLAG;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY_FLAG;
         return numbers[total--];
     }
 
