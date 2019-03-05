d6cb22f6858556e940fd802020373a7eaf8fd5ae
KovalykVolodymyr <kovalykvovapl@gmail.com>
2019-03-05 11:34:58 +0100

    Lab 3.1.5  sprawdzam za ctrl+alt+c

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int STATE_SIZE = 12;
+    public static final int NEGATIVE_NUMBER = -1;
+    public static final int FULL_NUMBER = 11;
+    public static final int ENTYTY_NUMBER = -1;
+    private int[] numbers = new int[STATE_SIZE];
 
-    public int total = -1;
+    public int total = ENTYTY_NUMBER;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,11 +16,11 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == NEGATIVE_NUMBER;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_NUMBER;
     }
 
     protected int peekaboo() {
