114cfaabac01b91ed6b9796541b4433f3eecd42e
Matix97 <mateusz_baranski@outlook.com>
2019-03-05 20:40:01 +0100

    3.1.6 Adding private, protected etc.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int STACK_CAPACITY = 12;
-    public static final int EMPTY = -1;
-    public static final int MAX_SIZE = 11;
+    private static final int STACK_CAPACITY = 12;
+    private static final int EMPTY = -1;
+    private static final int MAX_SIZE = 11;
     private int[] NUMBERS = new int[STACK_CAPACITY];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
