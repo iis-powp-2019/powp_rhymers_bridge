82dbb16b0f0ccc1851494296b49b845dccd20058
Matix97 <mateusz_baranski@outlook.com>
2019-03-05 20:33:59 +0100

    3.1.5 Extracting constant

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,7 +2,10 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    public static final int STACK_CAPACITY = 12;
+    public static final int EMPTY = -1;
+    public static final int MAX_SIZE = 11;
+    private int[] NUMBERS = new int[STACK_CAPACITY];
 
     public int total = -1;
 
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_SIZE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY;
         return NUMBERS[total--];
     }
 
