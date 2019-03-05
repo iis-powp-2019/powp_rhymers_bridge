a9a374fe2f392d07a7fd785692e73acdc4e542b5
astepaniuk <truemob2@gmail.com>
2019-03-05 11:15:30 +0100

    lab1_task_3.1.3

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,5 +1,5 @@
 package edu.kis.vh.nursery;
-
+//CTRL-SHIFT-BACKSPACE for going to last edited line
 public class FIFORhymer extends defaultCountingOutRhymer {
 
 	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
