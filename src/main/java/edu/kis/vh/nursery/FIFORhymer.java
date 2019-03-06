2ad40712c35d5444179bca6d729abddf2f678e89
Jakub <kuba.jakub17@gmail.com>
2019-03-06 15:07:00 +0100

    Ppunkt 1 i 2: naprawa formatowania (wcięcia i klamry)

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,20 +1,23 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
-
+public class FIFORhymer
+		extends defaultCountingOutRhymer
+{
+	
 	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
 	
 	@Override
-	public int countOut() {
-		while (!callCheck())
+	public int countOut()
+	{
+		while(!callCheck())
 			
-		temp.countIn(super.countOut());
+			temp.countIn(super.countOut());
 		
 		int ret = temp.countOut();
 		
-		while (!temp.callCheck())
+		while(!temp.callCheck())
 			
-		countIn(temp.countOut());
+			countIn(temp.countOut());
 		
 		return ret;
 	}
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,40 +1,47 @@
 package edu.kis.vh.nursery.list;
 
-public class IntLinkedList {
-
+public class IntLinkedList
+{
+	
 	Node last;
 	int i;
-
-	public void push(int i) {
-		if (last == null)
+	
+	public void push(int i)
+	{
+		if(last == null)
 			last = new Node(i);
-		else {
+		else
+		{
 			last.next = new Node(i);
 			last.next.prev = last;
 			last = last.next;
 		}
 	}
-
-	public boolean isEmpty() {
+	
+	public boolean isEmpty()
+	{
 		return last == null;
 	}
-
-	public boolean isFull() {
+	
+	public boolean isFull()
+	{
 		return false;
 	}
-
-	public int top() {
-		if (isEmpty())
+	
+	public int top()
+	{
+		if(isEmpty())
 			return -1;
 		return last.value;
 	}
-
-	public int pop() {
-		if (isEmpty())
+	
+	public int pop()
+	{
+		if(isEmpty())
 			return -1;
 		int ret = last.value;
 		last = last.prev;
 		return ret;
 	}
-
+	
 }
