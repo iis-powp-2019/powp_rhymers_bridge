c522bd05dfe749d735d520b0f0b63c7d10432f4b
Vandelin <polishotaku@gmail.com>
2019-03-06 15:30:42 +0100

    8- Used encapsulation on totalRejected field.

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -3,17 +3,25 @@ package edu.kis.vh.nursery;
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private static final int ZERO = 0;
-    int totalRejected = ZERO;
+    private int totalRejected = ZERO;
 
     int reportRejected() {
-        return totalRejected;
+        return getTotalRejected();
     }
 
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
-            totalRejected++;
+            setTotalRejected(getTotalRejected() + 1);
         else
             super.countIn(in);
+    }
+
+    public int getTotalRejected() {
+        return totalRejected;
+    }
+
+    public void setTotalRejected(int totalRejected) {
+        this.totalRejected = totalRejected;
     }
 }
