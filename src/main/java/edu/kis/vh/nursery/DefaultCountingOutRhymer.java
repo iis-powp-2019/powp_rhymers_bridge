6b788c497dcecd01fa148ea756566f9f71fa6e37
Vandelin <polishotaku@gmail.com>
2019-03-06 15:38:33 +0100

    5- Changed naming of constants.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,12 +3,14 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
 
-    private static final int ONE = -1;
-    private static final int ELEVEN = 11;
+    private static final int INITIAL_STACK_INDEX = -1;
+    private static final int RETURN_VALUE = -1;
+    private static final int FULL_STACK_SIZE = 11;
+    private static final int MAX_STACK_SIZE = 12;
 
-    private int[] numbers = new int[12];
+    private int[] numbers = new int[MAX_STACK_SIZE];
 
-    private int total = ONE;
+    private int total = INITIAL_STACK_INDEX;
 
     public int getTotal() {
         return total;
@@ -20,22 +22,22 @@ public class DefaultCountingOutRhymer {
     }
 
     boolean callCheck() {
-        return total == ONE;
+        return total == INITIAL_STACK_INDEX;
     }
 
     boolean isFull() {
-        return total == ELEVEN;
+        return total == FULL_STACK_SIZE;
     }
 
     int peekaboo() {
         if (callCheck())
-            return ONE;
+            return RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return ONE;
+            return RETURN_VALUE;
         return numbers[total--];
     }
 }
