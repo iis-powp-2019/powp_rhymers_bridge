1e6b97489014323b9cbd971876b35a4663032c73
msniec <michal.sniec@gmail.com>
2019-03-06 14:51:17 +0100

    2. Automatic code refactor

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -14,18 +14,20 @@ public class defaultCountingOutRhymer {
 	public boolean callCheck() {
 		return total == -1;
 	}
-		
+
 	public boolean isFull() {
 		return total == 11;
 	}
-	
+
 	protected int peekaboo() {
-		if (callCheck()) return -1;
+		if (callCheck())
+			return -1;
 		return NUMBERS[total];
 	}
-			
+
 	public int countOut() {
-		if (callCheck()) return -1;
+		if (callCheck())
+			return -1;
 		return NUMBERS[total--];
 	}
 }
