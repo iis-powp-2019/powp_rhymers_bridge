7a94e21fa20a16b609105660e2df47182c3af19a
mkarpacka <209348@edu.p.lodz.pl>
2019-03-06 15:38:48 +0100

    literaly zastapione deklaracjami stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,15 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int MAX_STACK_SIZE = 12;
 
-	public int total = -1;
+	private static final int RETURN_DEFAULT = -1;
+	
+	private static final int INITIAL_STACK_INDEX = -1;
+
+	private int[] numbers = new int[MAX_STACK_SIZE];
+
+	public int total = INITIAL_STACK_INDEX;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +18,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == INITIAL_STACK_INDEX;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == MAX_STACK_SIZE - 1;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return RETURN_DEFAULT;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return RETURN_DEFAULT;
 		return numbers[total--];
 	}
 
