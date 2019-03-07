43ac6cd72cbc74642bd5f620dec6d72ac2a575e7
209364 <209364@CARBON.CTI.P.LODZ.PL>
2019-03-07 15:46:29 +0100

    Wprowadzenie stałych wartości

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int MAXSIZE = 12;
+    public static final int DEFAULT_STACK_VALUE = -1;
+    public static final int MAX_STACK_VALUE = 11;
+    public static final int ERROR_NUMBER = -1;
+    private int[] numbers = new int[MAXSIZE];
 
-    public int total = -1;
+    public int total = DEFAULT_STACK_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == DEFAULT_STACK_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_STACK_VALUE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR_NUMBER;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_NUMBER;
         return numbers[total--];
     }
 
