871ff2374c6109aa4364d1d10ab9ed0da79bf3fd
Piotrek <piotr.marczewski95@gmail.com>
2019-03-07 18:23:02 +0100

    gettery i settery total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -23,6 +23,14 @@ public class DefaultCountingOutRhymer {
 		return total == 11;
 	}
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
 	protected int peekaboo() {
 		if (callCheck())
 			return error;
