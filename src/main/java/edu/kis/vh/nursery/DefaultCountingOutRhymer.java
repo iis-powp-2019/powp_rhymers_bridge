306b7260475d0349f48e7545d8cb27461882b419
wsobczyk <wiktoriasobczyk@gmail.com>
2019-03-07 19:03:09 +0100

    3.6 changes modificators

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,25 +3,25 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
 	private static final int DEFAULT_INDEX_STACK = -1;
-	
+
 	private static final int NOT_FOUND_RETURN = -1;
 
 	private static final int MAX_SIZE_STACK = 12;
 
 	private int[] numbers = new int[MAX_SIZE_STACK];
 
-	public int total = DEFAULT_INDEX_STACK;
+	private int total = DEFAULT_INDEX_STACK;
 
-	public void countIn(int in) {
+	protected void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
-	public boolean callCheck() {
+	protected boolean callCheck() {
 		return total == DEFAULT_INDEX_STACK;
 	}
 
-	public boolean isFull() {
+	protected boolean isFull() {
 		return total == MAX_SIZE_STACK-1;
 	}
 
@@ -31,7 +31,7 @@ public class DefaultCountingOutRhymer {
 		return numbers[total];
 	}
 
-	public int countOut() {
+	protected int countOut() {
 		if (callCheck())
 			return NOT_FOUND_RETURN;
 		return numbers[total--];
