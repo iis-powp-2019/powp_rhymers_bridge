5a8b57293b303af6fb9c295290c184943c95d1d9
209450 <209450@edu.p.lodz.pl>
2019-03-07 15:34:46 +0100

    4 + formatowanie plików w pakiecie "list"

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,18 +1,18 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public DefaultCountingOutRhymer defaultCountingOutRhymer = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
         while (!callCheck())
-            temp.countIn(super.countOut());
+            defaultCountingOutRhymer.countIn(super.countOut());
 
-        int ret = temp.countOut();
+        int ret = defaultCountingOutRhymer.countOut();
 
-        while (!temp.callCheck())
-            countIn(temp.countOut());
+        while (!defaultCountingOutRhymer.callCheck())
+            countIn(defaultCountingOutRhymer.countOut());
 
         return ret;
     }
