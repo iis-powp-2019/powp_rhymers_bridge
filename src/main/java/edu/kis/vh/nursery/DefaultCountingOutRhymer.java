bb2c47993456d296077884196a228580bb990933
Paxon96 <bwloczyk@gmail.com>
2019-03-09 12:48:37 +0100

    3.1.6 reduction fields visibility

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,27 +2,27 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int EMPTY_FIFO = -1;
-    public static final int FULL_FIFO = 11;
-    public static final int CAPACITY = 12;
+    private static final int EMPTY_FIFO = -1;
+    private static final int FULL_FIFO = 11;
+    private static final int CAPACITY = 12;
     private int[] numbers = new int[CAPACITY];
 
-    public int total = EMPTY_FIFO;
+    private int total = EMPTY_FIFO;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    boolean callCheck() {
         return total == EMPTY_FIFO;
     }
 
-    public boolean isFull() {
+    boolean isFull() {
         return total == FULL_FIFO;
     }
 
-    protected int peekaboo() {
+    int peekaboo() {
         if (callCheck())
             return EMPTY_FIFO;
         return numbers[total];
