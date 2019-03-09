9ecc49270e6a644bf358a74a624b4ee07db846bb
KovalykVolodymyr <kovalykvovapl@gmail.com>
2019-03-09 17:26:04 +0100

    3.1.5 last fix

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,6 +6,8 @@ public class DefaultCountingOutRhymer {
     private static final int NEGATIVE_NUMBER = -1;
     private static final int FULL_INDEX = 11;
     private static final int INDEX_EMPTY = -1;
+    public static final int NAMBER_OUT = -1;
+    public static final int NAMBER_PIKABO = -1;
     private int[] numbers = new int[STACK_MAX_SIZE];
 
     private int total = INDEX_EMPTY;
@@ -25,13 +27,13 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return NAMBER_PIKABO;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return NAMBER_OUT;
         return numbers[total--];
     }
 
