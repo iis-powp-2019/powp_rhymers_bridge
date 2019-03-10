c3596449d92c2698f399db586b67775be8b2480f
Adam Sylla <adamski126@gmail.com>
2019-03-10 19:18:47 +0100

    *3.1.7 wygenerowano getter dla pola total w klasie DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,6 +9,10 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_STACK_HEAD_INDEX;
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
