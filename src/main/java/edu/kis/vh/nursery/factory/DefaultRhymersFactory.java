c79b2e9bdb1ae7260ec96a9a8266ea22962b0219
astepaniuk <truemob2@gmail.com>
2019-03-10 19:08:36 +0100

    lab1_task_3.3.2 class documented

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,23 +4,38 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
+/**
+ * Factory for different implementations of Rhymers
+ */
 public class DefaultRhymersFactory implements RhymersFactory {
 
+	/**
+	 * @return DefaultCountingOutRhymer - default implementation of Rhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getStandardRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
+	/**
+	 * @return DefaultCountingOutRhymer - default implementation of Rhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFalseRhymer() {
 		return new DefaultCountingOutRhymer();
 	}
 
+	/**
+	 * @return FIFO-based implementation of Rhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getFIFORhymer() {
 		return new FIFORhymer();
 	}
 
+	/**
+	 * @return Hanoi-based implementation of Rhymer
+	 */
 	@Override
 	public DefaultCountingOutRhymer getHanoiRhymer() {
 		return new HanoiRhymer();
