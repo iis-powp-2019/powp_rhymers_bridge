34c82030ad6b90d1dd2cd28e0495306421674659
Mikołaj Baran <tarralan@wp.pl>
2019-03-10 22:08:14 +0100

    Zadanie 3.3.2

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -5,6 +5,11 @@ public class IntLinkedList {
     private Node last;
     private int i;
 
+    /**
+     *
+     * @param i
+     * Add i to linkedList
+     */
     private void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -15,20 +20,42 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Checks that list is empty
+     * @return
+     * True if last == null
+     * false if last !=null
+     */
     private boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Checks that list is full
+     * @return
+     * Always return false :)
+     */
     private boolean isFull() {
         return false;
     }
 
+    /**
+     *
+     * @return
+     * last value from linkedList
+     * if linkedList is empty return -1
+     */
     private int top() {
         if (isEmpty())
             return -1;
         return last.value;
     }
 
+    /**
+     *
+     * @return
+     * Last added value from linkedList and delete this value form list
+     */
     private int pop() {
         if (isEmpty())
             return -1;
