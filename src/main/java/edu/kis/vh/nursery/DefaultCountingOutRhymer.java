71810fa5b6974814e7afd47c125ed20413209b08
Mikołaj Baran <tarralan@wp.pl>
2019-03-10 21:16:35 +0100

    Zadanie 3.1.6

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,27 +2,27 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int STACK_LAST_POSITION = 11;
-    public static final int STACK_STARTING_POSITION = -1;
-    public static final int STACK_VALUE_ON_STARTING_POSITION = -1;
+    private static final int STACK_LAST_POSITION = 11;
+    private static final int STACK_STARTING_POSITION = -1;
+    private static final int STACK_VALUE_ON_STARTING_POSITION = -1;
     private int[] numbers = new int[12];
 
-    public int total = STACK_STARTING_POSITION;
+    private int total = STACK_STARTING_POSITION;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    boolean callCheck() {
         return total == STACK_STARTING_POSITION;
     }
 
-    public boolean isFull() {
+    boolean isFull() {
         return total == STACK_LAST_POSITION;
     }
 
-    protected int peekaboo() {
+    int peekaboo() {
         if (callCheck())
             return STACK_VALUE_ON_STARTING_POSITION;
         return numbers[total];
