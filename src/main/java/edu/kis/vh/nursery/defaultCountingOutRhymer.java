7c38d76396232942f33de703b48691cff53f72f4
Szymon Sobieszczyk <esesinho@gmail.com>
2019-03-10 11:27:26 +0100

    Wygenerowano getter zad7

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -5,8 +5,11 @@ public class DefaultCountingOutRhymer {
     private final int STACK_MAX_SIZE = 12;
     private final int INITIAL_STACK_INDEX=-1;
     private int[] NUMBERS = new int[STACK_MAX_SIZE];
-
     private int total = INITIAL_STACK_INDEX;
+
+    public int getTotal() {
+        return total;
+    }
 
     public void countIn(int in) {
         if (!isFull())
