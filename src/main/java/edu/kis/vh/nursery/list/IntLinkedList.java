d7463985e6d48315ddf47fea0d121d477c4fba8e
Zandros229 <ernest.figura548@gmail.com>
2019-03-11 15:45:45 +0100

    3.3.2 Add java Doc

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,36 +1,71 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ *Linked List of integers where inner element of this List are nodes
+ */
 public class IntLinkedList {
+    /**
+     * Inner class of node in Linked List
+     * elements of Linked List are nodes
+     */
     private class Node {
 
         private int value;
         private Node prev;
         private Node next;
 
+        /**
+         * Constructor of node
+         * @param i value of Node
+         */
         protected Node(final int i) {
             setValue(i);
         }
 
+        /**
+         * return value of node
+         * @return value of node as int
+         */
         public int getValue() {
             return value;
         }
 
+        /**
+         * Set new value of node where new value is value
+         * @param value new value of node
+         */
         public void setValue(final int value) {
             this.value = value;
         }
 
+        /**
+         * return previous node
+         * @return previous node of this node
+         */
         public Node getPrev() {
             return prev;
         }
 
+        /**
+         * Set previous node of this node
+         * @param prev new previous node
+         */
         public void setPrev(final Node prev) {
             this.prev = prev;
         }
 
+        /**
+         * Return next node of this node
+         * @return next node
+         */
         public Node getNext() {
             return next;
         }
 
+        /**
+         * Set next node of this node
+         * @param next new next node of this node
+         */
         public void setNext(final Node next) {
             this.next = next;
         }
@@ -40,6 +75,10 @@ public class IntLinkedList {
     private Node last;
     private int i;
 
+    /**
+     * add i to the top of Linked List
+     * @param i value to add to Linked List as int
+     */
     public void push(final int i) {
         if (getLast() == null)
             setLast(new Node(i));
@@ -50,20 +89,38 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Return true if Linked List is empty or false if is not
+     * @return true if empty false if not as boolean
+     */
     public boolean isEmpty() {
         return getLast() == null;
     }
 
+    /**
+     *
+     * @return always false
+     */
+    //TODO: needs to add max capacity of list
     public boolean isFull() {
         return false;
     }
 
+    /**
+     *return head element of the Linked List as int
+     * @return top element as int
+     */
     public int top() {
         if (isEmpty())
             return EMPTY;
         return getLast().getValue();
     }
 
+    /**
+     * Return -1 if Linked List is empty.
+     * Return and delete head element of the Linked List
+     * @return value of the head element of -1 if empty as int
+     */
     public int pop() {
         if (isEmpty())
             return EMPTY;
@@ -72,14 +129,26 @@ public class IntLinkedList {
         return ret;
     }
 
+    /**
+     * Return last element of the linked List
+     * @return last element as Node
+     */
     public Node getLast() {
         return last;
     }
 
+    /**
+     * Set last elemnt of the linked list
+     * @param last set last element as last
+     */
     public void setLast(final Node last) {
         this.last = last;
     }
 
+    /**
+     * Return value of i
+     * @return value of i as int
+     */
     public int getI() {
         return i;
     }
