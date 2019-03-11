66aafb93cb1c0c1f3a2346021825cb96c1b3fed9
hadi97 <hadielwazer97@gmail.com>
2019-03-11 20:19:18 +0100

    checking tests validation and adding documentation to class IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * This class is mainly made for creating a list that contains various nodes
+ */
 public class IntLinkedList {
 
     private Node last;
@@ -15,20 +18,36 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * This method checks if the list is empty
+     * @return true if the list is empty or false if it is not
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * This method checks if the the list full
+     * @return always false
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * This method returns the top value
+     * @return the value of the last element or -1 if the list is empty
+     */
     public int top() {
         if (isEmpty())
             return -1;
         return last.getValue();
     }
 
+    /**
+     * This method delete element from the list
+     * @return the value of the element that was deleted or -1 if the list is empty
+     */
     public int pop() {
         if (isEmpty())
             return -1;
