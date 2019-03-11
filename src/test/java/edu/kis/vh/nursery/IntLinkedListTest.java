a66dcd59c91ac003d31ddb5be160ed057cd1d40f
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-11 23:08:00 +0100

    *3.2.2 dodano test dla IntLinkedList

---
+++ src/test/java/edu/kis/vh/nursery/IntLinkedListTest.java
@@ -0,0 +1,56 @@
+package edu.kis.vh.nursery;
+
+import edu.kis.vh.nursery.list.IntLinkedList;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IntLinkedListTest {
+
+    @Test
+    public void isEmptyTest(){
+
+        IntLinkedList linkedList = new IntLinkedList();
+
+        Boolean result = linkedList.isEmpty();
+
+        Assert.assertTrue(result);
+
+        linkedList.push(1);
+        result = linkedList.isEmpty();
+
+        Assert.assertFalse(result);
+
+    }
+
+    @Test
+    public void topTest(){
+
+        IntLinkedList linkedList = new IntLinkedList();
+
+        final int[] TEST_VALUES = {3,2,1};
+
+        for(int value : TEST_VALUES){
+            linkedList.push(value);
+            Assert.assertEquals(value,linkedList.top());
+        }
+
+        Assert.assertNotEquals(4,linkedList.top());
+
+    }
+
+    @Test
+    public void popTest(){
+
+        IntLinkedList linkedList = new IntLinkedList();
+
+        final int TEST_VALUE = 1;
+
+        Assert.assertTrue(linkedList.isEmpty());
+
+        linkedList.push(TEST_VALUE);
+
+        Assert.assertFalse(linkedList.isEmpty());
+
+    }
+
+}
