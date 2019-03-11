05798a438539342b78d599f2f7cdb182435e14ee
Zandros229 <ernest.figura548@gmail.com>
2019-03-11 12:32:03 +0100

    3.1.8 encapsulation of non private attributes in class

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -3,16 +3,24 @@ package edu.kis.vh.nursery;
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private static final int ZERO = 0;
-    int totalRejected = ZERO;
+    private int totalRejected = ZERO;
 
     protected int reportRejected() {
-        return totalRejected;
+        return getTotalRejected();
     }
 
     protected void countIn(int in) {
         if (!callCheck() && in > peekaboo())
-            totalRejected++;
+            setTotalRejected(getTotalRejected() + 1);
         else
             super.countIn(in);
+    }
+
+    public int getTotalRejected() {
+        return totalRejected;
+    }
+
+    public void setTotalRejected(int totalRejected) {
+        this.totalRejected = totalRejected;
     }
 }
