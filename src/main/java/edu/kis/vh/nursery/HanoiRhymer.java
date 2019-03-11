e5067b81ce35911f101aac1ab8925724950ab7e0
Adam Sylla <adamski126@gmail.com>
2019-03-11 21:50:02 +0100

    *3.3.2 wyenerowano szkielet dokumentacji do klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -6,10 +6,18 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     //TODO: DELETE reportRejected, does the same thing as getTotalRejected
 
+    /**
+     * Return the total ammount of rejected values
+     * @return ammount of rejected values in int
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Adds values to the array if array isn't full or increment totalRejected if array is full
+     * @param in value to be added to the list
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
@@ -18,6 +26,10 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
             super.countIn(in);
     }
 
+    /**
+     * Return the total ammount of rejected values
+     * @return ammount of rejected values in int
+     */
     public int getTotalRejected() {
         return totalRejected;
     }
