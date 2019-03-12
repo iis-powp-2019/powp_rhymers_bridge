48e1aba8ccc38390c9b3f9e287a17db7d53bd0ce
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-03-12 19:52:23 +0100

    3.1.6 Widoczność

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,33 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int minusOne = -1;
-    public static final int eleven = 11;
-    public static final int twelve = 12;
+    private static final int minusOne = -1;
+    private static final int eleven = 11;
+    private static final int twelve = 12;
     private int[] numers = new int[twelve];
 
-	public int total = minusOne;
+    private int total = minusOne;
 
 	public void countIn(int in) {
 		if (!isFull())
 			numers[++total] = in;
 	}
 
-	public boolean callCheck() {
+	boolean callCheck() {
 		return total == minusOne;
 	}
 
-	public boolean isFull() {
+	boolean isFull() {
 		return total == eleven;
 	}
 
-	protected int peekaboo() {
+	int peekaboo() {
 		if (callCheck())
 			return minusOne;
 		return numers[total];
 	}
 
-	public int countOut() {
+	int countOut() {
 		if (callCheck())
 			return minusOne;
 		return numers[total--];
