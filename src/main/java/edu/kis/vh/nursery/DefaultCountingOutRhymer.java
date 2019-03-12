42a02b6a3fdefbb439fc299894fcd618242d01e0
wsobczyk <wiktoriasobczyk@gmail.com>
2019-03-12 11:17:49 +0100

    3.12* moving Node class

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,4 +1,35 @@
 package edu.kis.vh.nursery;
+class Node {
+
+	public int getValue() {
+		return value;
+	}
+
+
+	private int value;
+	private Node prev, next;
+
+	public Node(int i) {
+		value = i;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+}
 
 public class DefaultCountingOutRhymer {
 
