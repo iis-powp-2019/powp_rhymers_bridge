1be595265b2d76f605170e0b919515e66f5d44ab
Wivor <wiktor756@gmail.com>
2019-03-12 18:04:05 +0100

    3.3 Dodane komentarze do klasy IntArrayStack

--- src/main/java/edu/kis/vh/nursery/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -13,25 +13,45 @@ public class IntArrayStack {
         return total;
     }
 
+    /**
+     * If stack is not full add param 'in' to stack
+     * @param in number to add
+     */
     void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Check if stack empty
+     * @return 1 when is empty, 0 otherwise
+     */
     boolean callCheck() {
         return total == STARTING_POS;
     }
 
+    /**
+     * Check if stack is full
+     * @return 1 when is full, 0 otherwise
+     */
     boolean isFull() {
         return total == MAX_SIZE;
     }
 
+    /**
+     * Returns last added number to stack
+     * @return -1 when if stack is empty and number otherwise
+     */
     int peekaboo() {
         if (callCheck())
             return STARTING_POS;
         return numbers[total];
     }
 
+    /**
+     * Returns last added number to stack and drop it
+     * @return -1 when if stack is empty and number otherwise
+     */
     int countOut() {
         if (callCheck())
             return STARTING_POS;
