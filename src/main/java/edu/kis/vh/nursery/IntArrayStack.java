1c704d34cb74417796423f0793271c1f2beaa402
Filipowicz.Wiktor <209309@edu.p.lodz.pl>
2019-03-12 11:25:32 +0100

    lab 2 1. & 2. Brakujący plik

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,37 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+    private int CAPACITY = 12;
+    private int MAX_SIZE = CAPACITY - 1;
+    private int STARTING_POS = -1;
+
+
+    private int[] numbers = new int[CAPACITY];
+
+    public int total = STARTING_POS;
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == STARTING_POS;
+    }
+
+    public boolean isFull() {
+        return total == MAX_SIZE;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return STARTING_POS;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return STARTING_POS;
+        return numbers[total--];
+    }
+}
