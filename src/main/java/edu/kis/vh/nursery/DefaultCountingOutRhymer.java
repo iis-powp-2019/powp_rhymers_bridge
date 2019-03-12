54cd2f8a3639de840f3018eed7ef6d1237f4345d
Filipowicz.Wiktor <209309@edu.p.lodz.pl>
2019-03-12 10:40:05 +0100

    5. Zadeklarowanie stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private int CAPACITY = 12;
+    private int MAX_SIZE = CAPACITY - 1;
+    private int STARTING_POS = -1;
 
-    public int total = -1;
+
+    private int[] numbers = new int[CAPACITY];
+
+    public int total = STARTING_POS;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +17,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == STARTING_POS;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_SIZE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return STARTING_POS;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return STARTING_POS;
         /*komentarz w ostatnio edytowanym miejscu*/return numbers[total--];
     }
 
