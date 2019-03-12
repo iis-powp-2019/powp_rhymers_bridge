9202ee66a30bed238d68ab3178523d3f80c63fa8
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-03-12 19:21:28 +0100

    3.1.3 weryfikacja kombinacji kalwiszy

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -14,4 +14,5 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
 		else
 			super.countIn(in);
 	}
+	// skrót kalwiszowy daje nam możliwość przemieszczania między kratami na których pracujemy
 }
