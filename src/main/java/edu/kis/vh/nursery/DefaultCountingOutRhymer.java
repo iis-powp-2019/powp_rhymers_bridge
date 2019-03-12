d0684b722ff17bcda4559fce140c971342c525f4
RedMihalo <michal.21.96@gmail.com>
2019-03-12 23:19:44 +0100

    Generacja i uzupełnienie szkieletu dokumentacji (3.3)

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Class implementing stack
+ */
 public class DefaultCountingOutRhymer {
 
 	private static final int NUMBERS_QUANTITY = 12;
@@ -10,29 +13,50 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_STACK_INDICATOR;
 
+    /**
+     * @return amount of values currently on stack
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * Put value on stack
+     * @param in value to put
+     */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+    /**
+     * Checks if stack is empty
+     * @return true if stack is empty
+     */
 	boolean isEmpty() {
 		return total == EMPTY_STACK_INDICATOR;
 	}
 
+    /**
+     * Checks if stack is full
+     * @return true if stack is full
+     */
 	boolean isFull() {
 		return total == FULL_STACK_INDICATOR;
 	}
 
+    /**
+     * @return value from the top of the stack without removing it
+     */
 	protected int peekaboo() {
 		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
 		return numbers[total];
 	}
 
+    /**
+     * @return value from the top of the stack and remove it
+     */
 	public int countOut() {
 		if (isEmpty())
 			return EMPTY_STACK_INDICATOR;
