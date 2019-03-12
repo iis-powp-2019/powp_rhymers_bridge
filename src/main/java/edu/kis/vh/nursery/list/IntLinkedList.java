092c2a2831ad04e8480222abfa580f9f66b1ad4e
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-03-12 20:41:15 +0100

    3.2.1 i 3.3.2  Szkielet dookumentacji dla wybranej klasy oraz walidacja testów

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,8 +1,12 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * @author tobia
+ * Klasa 
+ */
 public class IntLinkedList {
 
-	Node last;
+	private Node last;
 	int i;
 
 	public void push(int i) {
@@ -15,20 +19,36 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * Sprawdzanie listy
+	 * @return zwraca prawde jeśli lista jest pusta albo fałsz jeśli coś w niej jest
+	 */
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	/**
+	 * Sprawdza czy lista jest pełna
+	 * @return zawsze fałszywe
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * Zwraca top wartość
+	 * @return wartość ostatniego elementu  albo -1 jeśli jest pusta
+	 */
 	public int top() {
 		if (isEmpty())
 			return -1;
 		return last.getValue();
 	}
 
+	/**
+	 * Usuwanie elementu z listy
+	 * @return wartość elementu albo -1 jeśli jest pusty
+	 */
 	public int pop() {
 		if (isEmpty())
 			return -1;
