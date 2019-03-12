60c1c4a6d359755fe3a4ac9fe4f12d70623207e5
Filipowicz.Wiktor <209309@edu.p.lodz.pl>
2019-03-12 11:24:30 +0100

    lab 2 2. Klasa DefaultCountingOutRyhmer napisana od nowa

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,6 +2,33 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
+    private IntArrayStack intArrayStack;
 
+    public DefaultCountingOutRhymer(IntArrayStack stack) {
+        this.intArrayStack = stack;
+    }
 
+    public DefaultCountingOutRhymer() {
+        intArrayStack = new IntArrayStack();
+    }
+
+    public void countIn(int in) {
+        intArrayStack.countIn(in);
+    }
+
+    boolean callCheck() {
+        return intArrayStack.callCheck();
+    }
+
+    boolean isFull() {
+        return intArrayStack.isFull();
+    }
+
+    int peekaboo() {
+        return intArrayStack.peekaboo();
+    }
+
+    public int countOut() {
+        return intArrayStack.countOut();
+    }
 }
