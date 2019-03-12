79c714ea387859153dba207a44a8114374c9ffbf
Piotr Rogowski <209446@gmail.com>
2019-03-12 22:30:07 +0100

    3.3.2 - Generated JavaDoc diagram

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,6 +1,9 @@
 package edu.kis.vh.nursery.list;
 
 
+/**
+ * The type Int linked list.
+ */
 class IntLinkedList {
     private Node last;
     private int i;
@@ -19,16 +22,31 @@ class IntLinkedList {
         return last == null;
     }
 
+    /**
+     * Is full boolean.
+     *
+     * @return the boolean
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Top int.
+     *
+     * @return the int
+     */
     public int top() {
         if (isEmpty())
             return -1;
         return last.value;
     }
 
+    /**
+     * Pop int.
+     *
+     * @return the int
+     */
     public int pop() {
         if (isEmpty())
             return -1;
@@ -37,28 +55,62 @@ class IntLinkedList {
         return ret;
     }
 
+    /**
+     * Gets last.
+     *
+     * @return the last
+     */
     public Node getLast() {
         return last;
     }
 
     private class Node {
 
+        /**
+         * The Value.
+         */
         final int value;
+        /**
+         * The Prev.
+         */
         Node prev;
+        /**
+         * The Next.
+         */
         Node next;
 
+        /**
+         * Instantiates a new Node.
+         *
+         * @param i the
+         */
         Node(final int i) {
             value = i;
         }
 
+        /**
+         * Gets value.
+         *
+         * @return the value
+         */
         public int getValue() {
             return value;
         }
 
+        /**
+         * Gets prev.
+         *
+         * @return the prev
+         */
         public Node getPrev() {
             return prev;
         }
 
+        /**
+         * Gets next.
+         *
+         * @return the next
+         */
         public Node getNext() {
             return next;
         }
