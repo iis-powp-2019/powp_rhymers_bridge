f7a1c15fc03b74134187ce9189ad48c8ed4bef56
Filipowicz.Wiktor <209309@edu.p.lodz.pl>
2019-03-12 10:55:01 +0100

    lab 2 1. Skopiowanie implementacji stosu do IntArrayStack

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,38 +2,6 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int CAPACITY = 12;
-    private int MAX_SIZE = CAPACITY - 1;
-    private int STARTING_POS = -1;
 
-
-    private int[] numbers = new int[CAPACITY];
-
-    public int total = STARTING_POS;
-
-    public void countIn(int in) {
-        if (!isFull())
-            numbers[++total] = in;
-    }
-
-    public boolean callCheck() {
-        return total == STARTING_POS;
-    }
-
-    public boolean isFull() {
-        return total == MAX_SIZE;
-    }
-
-    protected int peekaboo() {
-        if (callCheck())
-            return STARTING_POS;
-        return numbers[total];
-    }
-
-    public int countOut() {
-        if (callCheck())
-            return STARTING_POS;
-        /*komentarz w ostatnio edytowanym miejscu*/return numbers[total--];
-    }
 
 }
