5774b2039645e7ed575b758777842e789235293f
Lucat0n <lukasz662@gmail.com>
2019-03-12 20:47:27 +0100

    6. Ograniczenie widoczności atrybutów klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,33 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int capacity = 12;
-    public static final int array_initial_index = -1;
-    public static final int max_size = capacity-1;
+    private static final int capacity = 12;
+    private static final int array_initial_index = -1;
+    private static final int max_size = capacity-1;
     private int[] numbers = new int[capacity];
 
-    public int head = array_initial_index;
+    private int head = array_initial_index;
 
-    public void countIn(int in) {
+    void countIn(int in) {
         if (!isFull())
             numbers[++head] = in;
     }
 
-    public boolean callCheck() {
+    boolean callCheck() {
         return head == array_initial_index;
     }
 
-    public boolean isFull() {
+    boolean isFull() {
         return head == max_size;
     }
 
-    protected int peekaboo() {
+    int peekaboo() {
         if (callCheck())
             return array_initial_index;
         return numbers[head];
     }
 
-    public int countOut() {
+    int countOut() {
         if (callCheck())
             return array_initial_index;
         return numbers[head--];
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,13 +2,13 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    private int totalRejected = 0;
 
-    public int reportRejected() {
+    int reportRejected() {
         return totalRejected;
     }
 
-    public void countIn(int in) {
+    void countIn(int in) {
         if (!callCheck() && in > peekaboo()) //źle sformatowana
             totalRejected++;
         else //źle sformatowana
