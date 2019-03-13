c96f9fe8c053802e43a5603f73ffb29722217a02
209420 <209420@edu.p.lodz.pl>
2019-03-13 14:48:30 +0100

    3.1.1

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -6,16 +6,16 @@ public class FIFORhymer extends defaultCountingOutRhymer {
 	
 	@Override
 	public int countOut() {
-		while (!callCheck())
-			
-		temp.countIn(super.countOut());
-		
+		while (!callCheck()) {
+			temp.countIn(super.countOut());
+		}
+
 		int ret = temp.countOut();
-		
-		while (!temp.callCheck())
-			
-		countIn(temp.countOut());
-		
+
+		while (!temp.callCheck()) {
+			countIn(temp.countOut());
+		}
+
 		return ret;
 	}
 }
