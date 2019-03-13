ac8800e4608e259c3338852628e82871d100d946
ostrowski.kamil <209418@edu.p.lodz.pl>
2019-03-13 15:09:55 +0100

    Wykonanie punktu numer 3.1.5

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,7 +2,10 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int MIN = -1;
+    public static final int MAX = 11;
+    public static final int SIZE_NUMBERS = 12;
+    private int[] numbers = new int[SIZE_NUMBERS];
 
     public int total = -1;
 
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == MIN;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return MIN;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return MIN;
         return numbers[total--];
     }
 
