6b354d4afa0c678d763c48f60c764a1162d12883
Lucat0n <lukasz662@gmail.com>
2019-03-13 16:59:28 +0100

    3.3.2 Dodane komentarze potrzebne do dokumentacji

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -10,29 +10,53 @@ public class DefaultCountingOutRhymer {
 
     private int total = array_initial_index;
 
+    /**
+     * Returns total amount of elements in stack
+     * @return -1 if stack is empty, otherwise total amount
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * Inserts a number into stack if the stack is not full
+     * @param in number you want to add to stack
+     */
     void countIn(final int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Checks if the stack is empty
+     * @return true if the stack empty, false if not
+     */
     boolean callCheck() {
         return total == array_initial_index;
     }
 
+    /**
+     * Checks if the stack is full
+     * @return true if the stack is full, false if not
+     */
     boolean isFull() {
         return total == max_size;
     }
 
+    /**
+     * Returns the top value of stack
+     * @return -1 if stack is empty, number if not empty
+     */
     int peekaboo() {
         if (callCheck())
             return array_initial_index;
         return numbers[total];
     }
 
+    /**
+     * Returns and deletes the top value of stack
+     * @return -1 if stack is empty, number if not empty
+     */
     int countOut() {
         if (callCheck())
             return array_initial_index;
