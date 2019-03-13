2da79973d79d3dd7cffa6cdf5c119ca4782db261
pallineczka <paulina.stelmasiak97@gmail.com>
2019-03-13 21:31:56 +0100

    Zadanie 12 usunięcie pliku Node.java

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,3 +38,34 @@ public class IntLinkedList {
 		return ret;
 	}
 }
+
+class Node {
+
+    private int value;
+
+    private Node prev, next;
+
+    public Node(int i) {
+        value = i;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
