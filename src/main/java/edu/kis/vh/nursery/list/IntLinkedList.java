7e288dd100d641532ae4ed0e01bd7ca1ac44ff2e
mkarpacka <209348@edu.p.lodz.pl>
2019-03-13 14:45:22 +0100

    3.1.12 przeniesienie klasy Node

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,5 +1,37 @@
 package edu.kis.vh.nursery.list;
 
+class Node {
+
+	private int value;
+	private Node prev, next;
+
+	public Node(int i) {
+		value = i;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+}
+
+
 public class IntLinkedList {
 
 	private static final int RETURNED_VALUE = -1;
