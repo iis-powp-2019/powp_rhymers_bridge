c8f9e77b77ff34d1ed677540a93659a889ebdf71
damon150196 <damian_golema@interia.eu>
2019-03-13 23:46:28 +0100

    3.1.13

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,9 +7,14 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		RhymersFactory factory = new DefaultRhymersFactory();
-		
+		testRhymers(factory);
+	}
+
+	private static void testRhymers(RhymersFactory factory) 
+	{
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
 		
@@ -21,7 +26,8 @@ class RhymersDemo {
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
 		
-		for (int i = 0; i < rhymers.length; i++) {
+		for (int i = 0; i < rhymers.length; i++) 
+		{
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
@@ -29,7 +35,6 @@ class RhymersDemo {
 		
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
 	}
 	
 }
