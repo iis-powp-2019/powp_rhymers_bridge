f5dc114a0620ca17047ab946ac7cb5b7660d13f6
Delicja05 <209296@edu.p.lodz.pl>
2019-03-13 15:10:08 +0100

    3.1.3 Kombinacja klawiszow do przechodzenia pomiedzy plikami wzgledem daty ich otwarcia

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -8,5 +8,11 @@ public class Node {
 	public Node(int i) {
 		value = i;
 	}
+	
+//	Skróty klawiszowe alt + -> oraz alt + <- służą
+//	do poruszania się pomiędzy ostatnio otwieranymi plikami
+//	alt + <- służy do przechodzenia do poprzednio otwartego pliku
+//	a alt + -> służy do przechodzenia do póniej otwartych plików w 
+//	stosunku do obecnie otwartego jezeli sie cofamy
 
 }
