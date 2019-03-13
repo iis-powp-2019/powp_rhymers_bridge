d4ec7b41aa61da6d38d7d3f6fb4264676f381f32
mkarpacka <209348@edu.p.lodz.pl>
2019-03-13 15:30:23 +0100

    poprawa nazw funkcji w DefaultRhymersFactory (3.1.4)

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -15,8 +15,8 @@ class RhymersDemo {
 	}
 
 	private static void testRhymers(Rhymersfactory factory) {
-		DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-				factory.GetFIFORhymer(), factory.GetHanoiRhymer() };
+		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
+				factory.getFIFORhymer(), factory.getHanoiRhymer() };
 
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
