d5e20860f286fd26ba00bfd2d37baa253235fe88
ostrowski.kamil <209418@edu.p.lodz.pl>
2019-03-13 15:17:45 +0100

    Wykonanie punktu numer 3.1.6 cz. 2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MIN = -1;
-    public static final int MAX = 11;
-    public static final int SIZE_NUMBERS = 12;
+    private static final int MIN = -1;
+    private static final int MAX = 11;
+    private static final int SIZE_NUMBERS = 12;
     private int[] numbers = new int[SIZE_NUMBERS];
 
-    public int total = -1;
+    private int total = -1;
 
     public int getTotal() {
         return total;
--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery.list;
 
 class Node {
 
-    public int value;
-    public Node prev, next;
+    int value;
+    Node prev, next;
 
     public Node(int i) {
         value = i;
