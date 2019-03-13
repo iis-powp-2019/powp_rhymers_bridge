3599d4697aac65dcc947a3cf96fcbb0e59e4b9c2
209278 <209278@edu.p.lodz.pl>
2019-03-13 14:51:24 +0100

    poprawki 3.1.2

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,19 +2,19 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends defaultCountingOutRhymer {
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
 
-	@Override
-	public int countOut() {
-		while (!callCheck()) {
-			temp.countIn(super.countOut());
-		}
-		int ret = temp.countOut();
+    @Override
+    public int countOut() {
+        while (!callCheck()) {
+            temp.countIn(super.countOut());
+        }
+        int ret = temp.countOut();
 
-		while (!temp.callCheck()) {
-			countIn(temp.countOut());
-		}
-		return ret;
-	}
+        while (!temp.callCheck()) {
+            countIn(temp.countOut());
+        }
+        return ret;
+    }
 }
 
