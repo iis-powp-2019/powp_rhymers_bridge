a2f059a4e542e67b6b902dbdfec85e0bb271a332
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-03-13 18:46:59 +0100

    3.7 getter and setter for total field

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -32,4 +32,11 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
