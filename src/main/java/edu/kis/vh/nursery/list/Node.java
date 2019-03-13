7dd721662e98a4063ca2efb5ea670dfd5c493322
Kamil Jurek <informator017@wp.pl>
2019-03-13 15:31:19 +0100

    3.1.8 Encapsulation not private fields.

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -3,10 +3,34 @@ package edu.kis.vh.nursery.list;
 public class Node {
 
     protected int value;
-    protected Node prev, next;
+    protected Node prev;
+    protected Node next;
 
     protected Node(int i) {
         value = i;
     }
 
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
