d8bc0a93a64655645913d109bd2df713d6851171
pallineczka <paulina.stelmasiak97@gmail.com>
2019-03-13 16:22:12 +0100

    Zadanie 5 dodanie stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	public static final int STACK_SIZE = 12;
+    public static final int STACK_VALUE = -1;
 
-	public int total = -1;
+    private int[] numbers = new int[STACK_SIZE];
+
+	public int total = STACK_VALUE;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == STACK_VALUE;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == STACK_SIZE - 1;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return STACK_VALUE;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return STACK_VALUE;
 		return numbers[total--];
 	}
 
