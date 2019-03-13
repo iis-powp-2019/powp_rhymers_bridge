30d215d9809e0d75eb377000bb545731259e779d
struszczak.kamila <209472@edu.p.lodz.pl>
2019-03-13 15:06:01 +0100

    3.1.4 poprawienie błędów konwencji nazewnictwa klas, metod i atrybutów

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     // kombinacje alt <- i alt -> pomagają poruszać się po otwartych kartach
 
