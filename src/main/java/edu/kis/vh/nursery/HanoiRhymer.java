2ef66fec3339337a1c026a8d54dee842e2350e89
struszczak.kamila <209472@edu.p.lodz.pl>
2019-03-13 14:58:10 +0100

    3.1.3 sprawdzenie działania kombinacji alt <- i alt ->

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,6 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
+    // kombinacje alt <- i alt -> pomagają poruszać się po otwartych kartach
+
     int totalRejected = 0;
 
     public int reportRejected() {
