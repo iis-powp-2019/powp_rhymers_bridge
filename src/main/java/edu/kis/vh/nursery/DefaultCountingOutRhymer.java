bdbc4f8607852f9cea75897535b4cf714720f2b0
Kamil Jurek <informator017@wp.pl>
2019-03-13 15:08:18 +0100

    Extracting numbers to constants.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int FULL_STACK_SIZE = 12;
+    public static final int EMPTY_STACK_SIZE = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[FULL_STACK_SIZE];
+    public int total = EMPTY_STACK_SIZE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -14,6 +16,7 @@ public class DefaultCountingOutRhymer {
     public boolean callCheck() {
         return total == -1;
     }
+
 
     public boolean isFull() {
         return total == 11;
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,9 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    public static final int NUMBER_OF_REJECTED_RHYMES = 0;
+
+    int totalRejected = NUMBER_OF_REJECTED_RHYMES;
 
     public int reportRejected() {
         return totalRejected;
