35e045a24d0d2d33f8e296cfae03b29ccf2eba97
Przemyslaw Prencel <przemyslaw.prencel@goodylabs.com>
2019-03-14 22:32:58 +0100

    3.3.2 IntLikedList class JavaDoc

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -39,6 +39,11 @@ public class IntLinkedList {
 
     private Node last;
 
+    /**
+     * Added Node to IntLikedList
+     *
+     * @param i value of Node that will be the last element on the list
+     */
     public void push(int i) {
         if (getLast() == null)
             setLast(new Node(i));
@@ -49,20 +54,40 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Method checks IntLikedList content
+     *
+     * @return true if IntLikedList is empty, or false if it is empty
+     */
     public boolean isEmpty() {
         return getLast() == null;
     }
 
+    /**
+     * Method checks i
+     *
+     * @return always return false
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Method gets top element of IntLikedList and do not remove it
+     *
+     * @return value of last element of IntLikedList or -1 if it is empty
+     */
     protected int top() {
         if (isEmpty())
             return -1;
         return getLast().getValue();
     }
 
+    /**
+     * Method gets top element of IntLikedList removes it from IntLinkList
+     *
+     * @return vale of last element of IntLikedList or -1 if  it is empty
+     */
     protected int pop() {
         if (isEmpty())
             return -1;
@@ -71,10 +96,19 @@ public class IntLinkedList {
         return ret;
     }
 
+    /**
+     * Method gets last Node value of IntLikedList
+     *
+     * @return last node  value of IntLikedList
+     */
     public Node getLast() {
         return last;
     }
 
+    /**
+     * Method set last param to the last field
+     * @param last Node class instance
+     */
     public void setLast(Node last) {
         this.last = last;
     }
