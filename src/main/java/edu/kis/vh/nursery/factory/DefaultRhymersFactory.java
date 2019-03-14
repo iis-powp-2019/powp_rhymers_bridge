b203fbd3790fa11332826ef7efe9c7f9a27c4cb1
Natalia Ciosek <n_ciosek@wp.pl>
2019-03-14 15:12:58 +0100

    Zadanie 3.1.4 poprawka

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -6,19 +6,19 @@ import edu.kis.vh.nursery.HanoiRhymer;
 
 public class DefaultRhymersFactory implements RhymersFactory {
 
-    @Override public DefaultCountingOutRhymer GetStandardRhymer() {
+    @Override public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
-    @Override public DefaultCountingOutRhymer GetFalseRhymer() {
+    @Override public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
-    @Override public DefaultCountingOutRhymer GetFIFORhymer() {
+    @Override public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
 
-    @Override public DefaultCountingOutRhymer GetHanoiRhymer() {
+    @Override public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
     }
 
