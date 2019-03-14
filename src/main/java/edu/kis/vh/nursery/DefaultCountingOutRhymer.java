1dc5cbbd105aef01a40f3e1b00eb1ccd62ca7dcb
209521 <209521@CARBON.CTI.P.LODZ.PL>
2019-03-14 15:28:42 +0100

    Zadanie 3.2 5 zamiana literalow na stale

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int TOTAL_VALUE = -1;
 
-    public int total = -1;
+    private static final int MAX_SIZE = 12;
+
+    private int[] numbers = new int[MAX_SIZE];
+
+    public int total = TOTAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull()) {
@@ -13,7 +17,7 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL_VALUE;
     }
 
     public boolean isFull() {
@@ -22,14 +26,14 @@ public class DefaultCountingOutRhymer {
 
     protected int peekaboo() {
         if (callCheck()) {
-            return -1;
+            return TOTAL_VALUE;
         }
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck()) {
-            return -1;
+            return TOTAL_VALUE;
         }
         return numbers[total--];
     }
