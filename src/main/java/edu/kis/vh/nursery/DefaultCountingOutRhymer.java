229807f46443be58a264732a1c8a5534dfac27c9
Dominik Rak <rakdominikrak@gmail.com>
2019-03-14 15:27:40 +0100

    podpunkt 3.1.5
    Wyciągnięte stałe

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int EMPTY_STACK_VALUE = -1;
 
-	public int total = -1;
+	private static final int STACK_CAPACITY = 12;
+
+	private int[] numbers = new int[STACK_CAPACITY];
+
+	public int total = EMPTY_STACK_VALUE;
 
 	public void countIn(int in) {
 		if (!isFull()) {
@@ -14,22 +18,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == EMPTY_STACK_VALUE;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == STACK_CAPACITY-1;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return EMPTY_STACK_VALUE;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck()) {
-			return -1;
+			return EMPTY_STACK_VALUE;
 		}
 		return numbers[total--];
 	}
