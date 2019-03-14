e56739670072a5fa9cf054931f899ea9f7691444
209489 <lukasztracz195@gmail.com>
2019-03-14 20:21:48 +0100

    12. found class unneceserely have to public and delete it

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -37,4 +37,33 @@ public class IntLinkedList {
         return ret;
     }
 
+    class Node {
+
+        private final int value;
+        private Node prev, next;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        Node getPrev() {
+            return prev;
+        }
+
+        void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        Node getNext() {
+            return next;
+        }
+
+        void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
