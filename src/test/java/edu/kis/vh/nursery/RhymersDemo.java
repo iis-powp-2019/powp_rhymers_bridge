7028ec422ac33c1e084cf2cf4799f0e3caad5db0
209489 <lukasztracz195@gmail.com>
2019-03-14 20:42:58 +0100

    13. clean class RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,21 +3,30 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
+import java.util.Random;
+
 class RhymersDemo {
 
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
+        testRhymers(factory);
+    }
 
-        DefaultCountingOutRhymer[] rhymers = {factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-                factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
+    private static void testRhymers(RhymersFactory factory) {
+        DefaultCountingOutRhymer[] rhymers = {
+                factory.GetStandardRhymer(),
+                factory.GetFalseRhymer(),
+                factory.GetFIFORhymer(),
+                factory.GetHanoiRhymer()
+        };
 
         for (int index = 1; index < 15; index++)
             for (int rhymerIndex = 0; rhymerIndex < 3; rhymerIndex++)
                 rhymers[rhymerIndex].countIn(index);
 
-        java.util.Random rn = new java.util.Random();
+        Random random = new Random();
         for (int index = 1; index < 15; index++)
-            rhymers[3].countIn(rn.nextInt(20));
+            rhymers[3].countIn(random.nextInt(20));
 
         for (DefaultCountingOutRhymer rhymer : rhymers) {
             while (!rhymer.callCheck())
@@ -27,7 +36,6 @@ class RhymersDemo {
 
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportOfRejected());
-
     }
 
 }
