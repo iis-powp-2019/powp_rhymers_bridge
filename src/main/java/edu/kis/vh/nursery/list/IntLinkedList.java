2c648034af7baec5a9dcccd1ee73e3966b679cc8
pawel10x <x10player10x@gmail.com>
2019-03-15 20:51:14 +0100

    usuniecie klasy 3.1.(12)

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,17 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+	private class Node {
+
+		final int value;
+		Node prev, next;
+
+		Node(final int i) {
+			value = i;
+		}
+
+	}
+
 	private Node last;
 	int i;
 
