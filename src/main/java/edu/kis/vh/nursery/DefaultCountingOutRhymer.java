c8b8c5d60469a879274e21ad270a46d52dadf291
pawel10x <x10player10x@gmail.com>
2019-03-15 20:18:30 +0100

    getter dla pola total 3.1.(7)

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -10,6 +10,10 @@ public class DefaultCountingOutRhymer {
 
 	private int total = EMPTY_STACK_VALUE;
 
+	public int getTotal() {
+		return total;
+	}
+
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
