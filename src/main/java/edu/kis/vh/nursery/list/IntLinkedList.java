6dcf6e7ea2a32c043972ffca8fc4398de05415e8
Dominik <rakdominikrak@gmail.com>
2019-03-16 13:48:55 +0100

    3.3.2 stworzona dokumentacja dla klasy IntLinkedList
    dodatkowo poprawiona nazwa klasy Rhymersfactory na RhymersFactory

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,27 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * @author Dominik
+ *Class implementing int doulby linked list
+ */
 public class IntLinkedList {
 
+	/**
+	 * Constant int field holding value of index in list when list is empty
+	 */
 	private static final int EMPTY_LIST_VALUE = -1;
+	/**
+	 * reference to last node in the list
+	 */
 	private Node last;
 	private int i;
 
+	
+	
+	/**
+	 * adds new Node to the list
+	 * @param i int value assigned to Node
+	 */
 	public void push(final int i) {
 		if (last == null)
 			last = new Node(i);
@@ -16,20 +32,34 @@ public class IntLinkedList {
 		}
 	}
 
+	/**
+	 * @return true if list is empty, otherwise false
+	 */
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	
+	/**
+	 * @return true if list is full, otherwise false
+	 */
 	public boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * @return value of the node lastly added to list
+	 */
 	public int top() {
 		if (isEmpty())
 			return EMPTY_LIST_VALUE;
 		return last.getValue();
 	}
 
+	/**
+	 * removes lastly added node from the list
+	 * @return value of the node lastly added to list
+	 */
 	public int pop() {
 		if (isEmpty())
 			return EMPTY_LIST_VALUE;
@@ -38,32 +68,65 @@ public class IntLinkedList {
 		return ret;
 	}
 	
+	/**
+	 *Class implemnting Node used in list
+	 */
 	private class Node {
 
+		/**
+		 * value holded by the node
+		 */
 		private final int value;
+		/**
+		 * reference to the next node in the list
+		 */
 		private Node prev;
+		/**
+		 * reference to the previous node in the list
+		 */
 		private Node next;
 
+		/**
+		 * constructor of the Node 
+		 * @param i value assigned to the Node
+		 */
 		public Node(final int i) {
 			value = i;
 		}
 
+		/**
+		 * @return value assigned to the Node
+		 */
 		public int getValue() {
 			return value;
 		}
 
+		/**
+		 * @return reference to previous Node
+		 */
 		public Node getPrev() {
 			return prev;
 		}
 
+		/**
+		 * assigns reference to previous Node
+		 * @param prev reference to previous Node
+		 */
 		public void setPrev(final Node prev) {
 			this.prev = prev;
 		}
 
+		/**
+		 * @return reference to next Node
+		 */
 		public Node getNext() {
 			return next;
 		}
 
+		/**
+		 *assigns reference to next Node
+		 * @param prev reference to next Node
+		 */
 		public void setNext(final Node next) {
 			this.next = next;
 		}
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,18 +1,18 @@
 package edu.kis.vh.nursery;
 
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
-import edu.kis.vh.nursery.factory.Rhymersfactory;
+import edu.kis.vh.nursery.factory.RhymersFactory;
 //alt + <-- goes back to latest edited file, alt + --> reverse of previous one
 class RhymersDemo {
 
 	public static void main(String[] args) {
-		Rhymersfactory factory = new DefaultRhymersFactory();
+		RhymersFactory factory = new DefaultRhymersFactory();
 
 		testRhymers(factory);
 
 	}
 
-	public static void testRhymers(Rhymersfactory factory) {
+	public static void testRhymers(RhymersFactory factory) {
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer() };
 
