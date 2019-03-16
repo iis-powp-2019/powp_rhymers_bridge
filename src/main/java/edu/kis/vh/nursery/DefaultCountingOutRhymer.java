503bbf1eaefd99c38e3970f2ba1378134a6576e5
Leherguargin <Leherguargin@users.noreply.github.com>
2019-03-16 20:22:18 +0100

    3.1.7 generacja gettera i settera

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,6 +6,14 @@ public class DefaultCountingOutRhymer {
 	public static final int DWANASCIE = 12;
 	private int[] NUMBERS = new int[DWANASCIE];
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
 	public int total = MINUS_JEDEN;
 
 	public void countIn(int in) {
