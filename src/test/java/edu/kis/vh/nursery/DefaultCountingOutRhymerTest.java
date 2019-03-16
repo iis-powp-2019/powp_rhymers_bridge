5d026e3b134e6b458f58714d969ab4e8c6ae0e49
Natalia Ciosek <n_ciosek@wp.pl>
2019-03-16 14:05:35 +0100

    3.2.2 zmiana nazwy klasy RhymersJUnitTest na DefaultCountingOutRhymerTest

--- src/test/java/edu/kis/vh/nursery/DefaultCountingOutRhymerTest.java
+++ src/test/java/edu/kis/vh/nursery/DefaultCountingOutRhymerTest.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class RhymersJUnitTest {
+public class DefaultCountingOutRhymerTest {
 
 	@Test
 	public void testCountIn() {
