6352ada882ecda34be8268beed548a98ee29b72a
Natalia Ciosek <n_ciosek@wp.pl>
2019-03-16 14:12:29 +0100

    3.2.2 dodanie testu FIFORhymerTest

---
+++ src/test/java/edu/kis/vh/nursery/FIFORhymerTest.java
@@ -0,0 +1,26 @@
+package edu.kis.vh.nursery;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FIFORhymerTest {
+    @Test public void countOut() {
+        DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
+        final int DEFAUL_STACK_VALUE = -1;
+
+        int result = rhymer.countOut();
+        Assert.assertEquals(DEFAUL_STACK_VALUE, result);
+
+        int testVal = 1;
+        rhymer.countIn(testVal);
+
+        result = rhymer.countOut();
+        Assert.assertEquals(testVal , result);
+        result = rhymer.countOut();
+        Assert.assertEquals(DEFAUL_STACK_VALUE, result);
+
+    }
+
+}
