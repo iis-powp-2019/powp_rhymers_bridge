ef4e67fe598a622e957fe9dbd765b9a20c9ab636
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-03-16 18:32:14 +0100

    3.1.5. wygenerowanie stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,33 +2,37 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numebrs = new int[12];
+    public static final int CAPACITY = 12;
+    public static final int STACK_EMPTY_VALUE = -1;
+    public static final int STACK_FULL_VALUE = 11;
 
-    public int total = -1;
+    private int[] NUMBERS = new int[CAPACITY];
+
+    public int STACK_EMPTY_INDICATOR = STACK_EMPTY_VALUE; //total wskazuje ostatni element
 
     public void countIn(int in) {
         if (!isFull())
-            numebrs[++total] = in;
+            NUMBERS[++STACK_EMPTY_INDICATOR] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return STACK_EMPTY_INDICATOR == STACK_EMPTY_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return STACK_EMPTY_INDICATOR == STACK_FULL_VALUE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
-        return numebrs[total];
+            return STACK_EMPTY_VALUE;
+        return NUMBERS[STACK_EMPTY_INDICATOR];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
-        return numebrs[total--];
+            return STACK_EMPTY_VALUE;
+        return NUMBERS[STACK_EMPTY_INDICATOR--];
     }
 
 }
