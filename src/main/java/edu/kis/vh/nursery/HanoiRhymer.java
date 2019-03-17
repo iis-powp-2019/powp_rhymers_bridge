d4458850a9b51e8bd52a6a27402dde7ac04feecc
Kamil Jurek <informator017@wp.pl>
2019-03-17 12:34:43 +0100

    3.3.2 Generate element comment for HanoiRhymer class.

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,15 +1,24 @@
 package edu.kis.vh.nursery;
 
+/**
+ *
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private static final int NUMBER_OF_REJECTED_RHYMES = 0;
 
     int totalRejected = NUMBER_OF_REJECTED_RHYMES;
 
+    /**
+     * @return number of rejected rhymes
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * @param in 
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
