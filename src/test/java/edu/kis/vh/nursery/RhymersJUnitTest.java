7208dfc4607d9f575aeaced16500899062a5d9b0
Olena Hrynevych <211643@edu.p.lodz.pl>
2019-03-18 00:38:13 +0100

    zad 3.2.1

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+//all test are working properly
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
