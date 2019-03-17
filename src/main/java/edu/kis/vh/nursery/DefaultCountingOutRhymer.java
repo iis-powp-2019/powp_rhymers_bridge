5ec573de281ba8fb4cc068e859d1272b1de7a845
dettlaffb <209295@edu.p.lodz.pl>
2019-03-17 16:09:11 +0100

    3.3.2 Genereted elements for JavaDoc

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,30 +2,46 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
+
     private static final int STACK_CAPACITY = 12;
     private static final int EMPTY_STACK_VALUE = -1;
 
     private int[] numbers = new int[STACK_CAPACITY];
     private int total = -1;
 
+    /**
+     * @return Current value of total variable
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     *@param in Value which is added to numbers table.
+     */
     public void countIn(int in) {
         if (!isFull()){
             numbers[++total] = in;
         }
     }
 
+    /**
+     * @return {@code true} if stack is empty; {@code false} if stack contains some values
+     */
     boolean isEmpty() {
         return total == EMPTY_STACK_VALUE;
     }
 
+    /**
+     * @return {@code true} if stack is full; {@code false} if stack is not full
+     */
     boolean isFull() {
         return total == STACK_CAPACITY-1;
     }
 
+    /**
+     * @return {@code int} -1 if stack is empty {@code int} current element from numbers table
+     */
     int currentNumber() {
         if (isEmpty()){
             return EMPTY_STACK_VALUE;
@@ -34,6 +50,9 @@ public class DefaultCountingOutRhymer {
         }
     }
 
+    /**
+     * @return {@code int} -1 if stack is empty {@code int} element from numbers table until it is empty
+     */
     public int countOut() {
         if (isEmpty()) {
             return EMPTY_STACK_VALUE;
