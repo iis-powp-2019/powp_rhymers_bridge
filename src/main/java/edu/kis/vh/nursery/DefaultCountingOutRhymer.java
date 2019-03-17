aec0fedb488eea86d6519a60db618743d5779ab7
Olena Hrynevych <211643@edu.p.lodz.pl>
2019-03-17 20:56:10 +0100

    zad 3.1.5

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,8 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
-    public int total = -1;
+    public static final int STACK_CAPACITY = 12;
+    public static final int ERROR_CODE = -1;
+    public static final int MAX_SIZE = 11;
+    public static final int EMPTY_POSITION = -1;
+
+    private int[] NUMBERS = new int[STACK_CAPACITY];
+    public int total = EMPTY_POSITION;
 
     public void countIn(int in) {
         if (!isFull())
@@ -11,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERROR_CODE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_SIZE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR_CODE;
         return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_CODE;
         return NUMBERS[total--];
     }
 
--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,19 +5,22 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
+    public static final int MAX_SIZE = 3;
+    public static final int SIZE = 15;
+
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
 
         DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < SIZE; i++)
+            for (int j = 0; j < MAX_SIZE; j++)
                 rhymers[j].countIn(i);
 
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < SIZE; i++)
+            rhymers[MAX_SIZE].countIn(rn.nextInt(20));
 
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
@@ -26,7 +29,7 @@ class RhymersDemo {
         }
 
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[MAX_SIZE]).reportRejected());
 
     }
 
