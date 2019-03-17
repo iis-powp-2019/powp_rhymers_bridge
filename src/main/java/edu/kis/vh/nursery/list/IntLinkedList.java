a12c70a4ceec2ae4061d46a02b2208ae9732fd03
Natalia Ciosek <n_ciosek@wp.pl>
2019-03-17 22:56:19 +0100

    3.3.2 wygenerowanie szkieletu dokumentacji

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -43,6 +43,11 @@ public class IntLinkedList {
         return EMPTY;
     }
 
+    /**
+     * Added value to IntLikedList
+     *
+     * @param i this is the value that will be added to IntLikedList
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -53,20 +58,31 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Function of check if IntLikedList is empty
+     *
+     * @return true value if IntLikedList is empty otherwise false
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
-    public boolean isFull() {
-        return false;
-    }
-
+    /**
+     * This function get a top element of IntLikedList without delete it.
+     *
+     * @return top element of IntLikedList
+     */
     public int top() {
         if (isEmpty())
             return getEMPTY();
         return last.getValue();
     }
 
+    /**
+     * This function get top element of IntLikedList and delete that element from IntLinkList
+     *
+     * @return top element of IntLikedList
+     */
     public int pop() {
         if (isEmpty())
             return getEMPTY();
@@ -75,6 +91,11 @@ public class IntLinkedList {
         return ret;
     }
 
+    /**
+     * Get the last node  of IntLikedList
+     *
+     * @return last node  of IntLikedList
+     */
     public Node getLast() {
         return last;
     }
