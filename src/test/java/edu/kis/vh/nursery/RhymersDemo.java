91fa226be330e1a229e94d260ad5d2eddcd3e6f3
Patryk Gitner <209317@edu.p.lodz.pl>
2019-03-18 21:45:32 +0100

    13) Podział metody testRhymers na mniejsze oraz refaktor pętli for

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -16,26 +16,34 @@ class RhymersDemo {
     private static void testRhymers(RhymersFactory factory) {
         DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(), factory.getFIFORhymer(),
                 factory.getHanoiRhymer()};
+        countInAllRhymers(rhymers);
+        generateNumbersForCountInForHanoiRhymer(rhymers[3]);
+        countOutAllRhymers(rhymers);
+        System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
+    }
 
+    private static void countOutAllRhymers(DefaultCountingOutRhymer[] rhymers) {
+        for (DefaultCountingOutRhymer rhymer: rhymers) {
+            while (!rhymer.callCheck()) {
+                System.out.println(rhymer.countOut() + "  ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private static void generateNumbersForCountInForHanoiRhymer(DefaultCountingOutRhymer rhymer) {
+        Random random = new Random();
+        for (int i = 1; i < 15; i++) {
+            rhymer.countIn(random.nextInt(20));
+        }
+    }
+
+    private static void countInAllRhymers(DefaultCountingOutRhymer[] rhymers) {
         for (int i = 1; i < 15; i++) {
             for (int j = 0; j < 3; j++) {
                 rhymers[j].countIn(i);
             }
         }
-
-        Random random = new Random();
-        for (int i = 1; i < 15; i++) {
-            rhymers[3].countIn(random.nextInt(20));
-        }
-
-        for (int i = 0; i < rhymers.length; i++) {
-            while (!rhymers[i].callCheck()) {
-                System.out.print(rhymers[i].countOut() + "  ");
-            }
-
-            System.out.println();
-        }
-
-        System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
     }
 }
