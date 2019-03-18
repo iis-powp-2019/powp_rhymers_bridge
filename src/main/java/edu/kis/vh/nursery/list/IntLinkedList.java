1e32a431bf84b1eae318a42216300fa6ba24a247
209347 <209347@edu.p.lodz.pl>
2019-03-19 00:28:17 +0100

    3.3.2 - Dokumentacja klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,20 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Stack of integers
+ */
 public class IntLinkedList {
 
 	private static final int STACK_EMPTY = -1;
+	/**
+	 * Last value of the stack
+	 */
 	private Node last;
 	//int i;
 
+	/**
+	 * @param i element to be pushed
+	 */
 	private void push(int i) {
 		if (last == null)
 			last = new Node(i);
@@ -16,23 +25,36 @@ public class IntLinkedList {
 		}
 	}
 
+
+	/**
+	 * @return returns true if the stack is empty, false if stack is not empty
+	 */
 	private boolean isEmpty() {
 		return last == null;
 	}
 
+	/**
+	 * @return returns true if the stack is full, false if stack is not full
+	 */
 	private boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * @return returns the value of the value at the top of the stack if the stack is not empty. If the stack is empty it returns -1
+	 */
 	private int top() {
 		if (isEmpty())
 			return STACK_EMPTY;
 		return last.getValue();
 	}
 
+	/**
+	 * @return returns the value of the top of the stack and cuts it our of the stack. If the stack is empty it returns -1
+	 */
 	private int pop() {
 		if (isEmpty())
-			return -1;
+			return STACK_EMPTY;
 		int ret = last.getValue();
 		last = last.getPrev();
 		return ret;
