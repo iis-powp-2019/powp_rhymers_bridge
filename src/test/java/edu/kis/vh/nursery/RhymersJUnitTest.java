c960385ca838b2b069f3a232ebb425348b91196b
Piotr Plecinski <209432@edu.p.lodz.pl>
2019-03-18 21:44:02 +0100

    3.2 Poprawa i dodanie testow jednostkowych

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -1,5 +1,6 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.list.IntLinkedList;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,6 +14,21 @@ public class RhymersJUnitTest {
 
 		int result = rhymer.peekaboo();
 		Assert.assertEquals(testValue, result);
+
+
+		HanoiRhymer hanoiRhymer = new HanoiRhymer();
+		int secondTestValue = 5, thirdTestValue = 3;
+
+		hanoiRhymer.countIn(testValue);
+		hanoiRhymer.countIn(secondTestValue);
+
+		result = hanoiRhymer.peekaboo();
+		Assert.assertEquals(testValue, result);
+
+		hanoiRhymer.countIn(thirdTestValue);
+
+		result = hanoiRhymer.peekaboo();
+		Assert.assertEquals(thirdTestValue, result);
 	}
 
 	@Test
@@ -62,17 +78,101 @@ public class RhymersJUnitTest {
 	public void testCountOut() {
 		DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
 		final int EMPTY_STACK_VALUE = -1;
+		int testValue = 4, secondTestValue = 5;
 
 		int result = rhymer.countOut();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
 
-		int testValue = 4;
 		rhymer.countIn(testValue);
 
 		result = rhymer.countOut();
 		Assert.assertEquals(testValue, result);
 		result = rhymer.countOut();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+
+
+		FirstInFirstOutRhymer FIFORhymer = new FirstInFirstOutRhymer();
+
+		result = FIFORhymer.countOut();
+		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+
+		FIFORhymer.countIn(testValue);
+		FIFORhymer.countIn(secondTestValue);
+
+		result = FIFORhymer.countOut();
+		Assert.assertEquals(4,result);
+		result = FIFORhymer.countOut();
+		Assert.assertEquals(5,result);
+
+	}
+
+	@Test
+	public void testIsEmpty() {
+		IntLinkedList intLinkedList = new IntLinkedList();
+		final int EMPTY_STACK_VALUE = -1;
+		int testValue = 4;
+
+		boolean result = intLinkedList.isEmpty();
+		Assert.assertEquals(true,result);
+
+		intLinkedList.push(testValue);
+
+		result = intLinkedList.isEmpty();
+		Assert.assertEquals(false,result);
+
+		intLinkedList.pop();
+
+		result = intLinkedList.isEmpty();
+		Assert.assertEquals(true,result);
+	}
+
+	@Test
+	public void testPop() {
+		IntLinkedList intLinkedList = new IntLinkedList();
+		final int EMPTY_STACK_VALUE = -1;
+		int testValue = 4;
+
+		int result = intLinkedList.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE,result);
+
+		intLinkedList.push(testValue);
+
+		result = intLinkedList.pop();
+		Assert.assertEquals(testValue,result);
+
+		result = intLinkedList.pop();
+		Assert.assertEquals(EMPTY_STACK_VALUE,result);
+	}
+
+	@Test
+	public void testTop() {
+		IntLinkedList intLinkedList = new IntLinkedList();
+		final int EMPTY_STACK_VALUE = -1;
+		int testValue = 4;
+
+		int result = intLinkedList.top();
+		Assert.assertEquals(EMPTY_STACK_VALUE,result);
+
+		intLinkedList.push(testValue);
+
+		result = intLinkedList.top();
+		Assert.assertEquals(testValue,result);
+
+		intLinkedList.pop();
+
+		result = intLinkedList.top();
+		Assert.assertEquals(EMPTY_STACK_VALUE,result);
+	}
+
+	@Test
+	public void testPush() {
+		IntLinkedList intLinkedList = new IntLinkedList();
+		int testValue = 4, secondTestValue = 5;
+		intLinkedList.push(testValue);
+		intLinkedList.push(secondTestValue);
+
+		int result = intLinkedList.top();
+		Assert.assertEquals(secondTestValue,result);
 	}
 
 }
