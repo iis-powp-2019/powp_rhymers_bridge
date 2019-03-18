225a4bca23ca0dff6494de3df4f8d69623b04e42
Rahly <karol.oleksy97@gmail.com>
2019-03-18 22:34:40 +0100

    3.1.4 Poprawienie błędu konwencji nazewniczej klasy DefaultCountingOutRhymer która nie została zauważona wcześniej

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,10 +1,11 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
-    @Override public int countOut() {
+    @Override
+    public int countOut() {
         while (!callCheck())
 
             temp.countIn(super.countOut());
