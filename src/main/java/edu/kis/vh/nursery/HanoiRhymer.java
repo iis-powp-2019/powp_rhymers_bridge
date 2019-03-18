c1dd3df8c2bd3a9b6a009a73123168df1b089016
Patryk Gitner <209317@edu.p.lodz.pl>
2019-03-18 22:17:41 +0100

    3.3.2 - javadoc dla wybranej klasy i jej metod

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,24 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa HanoiRhymer, która dziedziczy z klasy DefaultCountingOutRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     * Metoda reportuje całkowitą liczbę odrzuconych liczb.
+     * @return totalRejected
+     */
     int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     * Metoda odziedziczona z klasy DefaultCountingOutRhymer
+     * @param in - niemutowalny parametr używany w metodzie
+     */
     @Override
     public void countIn(final int in) {
         if (!callCheck() && in > actualNumber()) {
@@ -16,9 +27,5 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
         else {
             super.countIn(in);
         }
-    }
-
-    public int getTotalRejected() {
-        return totalRejected;
     }
 }
