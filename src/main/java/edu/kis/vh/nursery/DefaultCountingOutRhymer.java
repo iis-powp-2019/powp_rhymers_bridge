8b9d87fcf5c4897c0d870520d531e4f81ef0640f
Piotr <amropz@gmail.com>
2019-03-19 00:44:41 +0100

    3.1.6

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,28 +8,28 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[CAPACITY];
 
-    public int total = EMPTY;
+    private int total = EMPTY;
 
-    public void countIn(int in) {
+    void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    boolean callCheck() {
         return total == -1;
     }
 
-    public boolean isFull() {
+    boolean isFull() {
         return total == MAX_INDEX;
     }
 
-    protected int peekaboo() {
+    int peekaboo() {
         if (callCheck())
             return -1;
         return numbers[total];
     }
 
-    public int countOut() {
+    int countOut() {
         if (callCheck())
             return -1;
         return numbers[total--];
