637540bb5183764c0935714b31ea2750ed53dbdb
MichałNawrot <michal.nawrot.mn.mn@gmail.com>
2019-03-18 20:21:09 +0100

    zadanie 3.3.2

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,29 +2,59 @@ package edu.kis.vh.nursery.list;
 
 class Node {
 
+    protected int value;
+    protected Node prev, next;
+
+    /**
+     * Getting value of Node
+     *
+     * @return value of Node
+     */
     public int getValue() {
         return value;
     }
 
-    protected int value;
-    protected Node prev, next;
-
+    /**
+     * Constructor of Node with 1 param
+     *
+     * @param i - value to add
+     */
     public Node(int i) {
         value = i;
     }
 
+    /**
+     * Getting previous Node
+     *
+     * @return previous Node
+     */
     public Node getPrev() {
         return prev;
     }
 
+    /**
+     * Setting previous Node
+     *
+     * @param prev - Node to set as previous
+     */
     public void setPrev(Node prev) {
         this.prev = prev;
     }
 
+    /**
+     * Getting next Node
+     *
+     * @return next Node
+     */
     public Node getNext() {
         return next;
     }
 
+    /**
+     * Setting next Node
+     *
+     * @param next - Node to set as next
+     */
     public void setNext(Node next) {
         this.next = next;
     }
@@ -34,38 +64,63 @@ public class IntLinkedList {
 
     private static final int DEFAULT_INDEX = -1;
     Node last;
-	int i;
+    int i;
 
-	public void push(int i) {
-		if (last == null)
-			last = new Node(i);
-		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
-		}
-	}
+    /**
+     * Add value to the Node
+     *
+     * @param i - will be added to the Node
+     */
+    public void push(int i) {
+        if (last == null)
+            last = new Node(i);
+        else {
+            last.next = new Node(i);
+            last.next.prev = last;
+            last = last.next;
+        }
+    }
 
-	public boolean isEmpty() {
-		return last == null;
-	}
+    /**
+     * Checking whether LinkedList is empty or not
+     *
+     * @return true if empty
+     */
+    public boolean isEmpty() {
+        return last == null;
+    }
 
-	public boolean isFull() {
-		return false;
-	}
+    /**
+     * Checking whether LinkedList is full
+     *
+     * @return false if is full
+     */
+    public boolean isFull() {
+        return false;
+    }
 
-	public int top() {
-		if (isEmpty())
-			return DEFAULT_INDEX;
-		return last.value;
-	}
+    /**
+     * Getting top element form the LinkedList
+     *
+     * @return top element of Linkedlist
+     */
+    public int top() {
+        if (isEmpty())
+            return DEFAULT_INDEX;
+        return last.value;
+    }
 
-	public int pop() {
-		if (isEmpty())
-			return DEFAULT_INDEX;
-		int ret = last.value;
-		last = last.prev;
-		return ret;
-	}
+    /**
+     * Getting and deleting top element from the LinkedList
+     *
+     * @return top element of LinkedList
+     */
+    public int pop() {
+        if (isEmpty())
+            return DEFAULT_INDEX;
+        int ret = last.value;
+        last = last.prev;
+        return ret;
+    }
 
 }
