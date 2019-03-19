cc9c25c6dfb29c88bb3c2abfd7a6aabef3c6caa7
Jakub Zawiasa <zawiasa.jakub@gmail.com>
2019-03-19 14:57:54 +0100

    Zadanie 3.3 2 Dokumentacja klasy

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,79 +1,140 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Class implementing linked list
+ */
 public class IntLinkedList {
 
-    private static final int EMPTY = -1;
-    private Node last;
-    private int i;
+	/**
+	 * Empty list indicator
+	 */
+	private static final int EMPTY = -1;
+	/**
+	 * Last node reference
+	 */
+	private Node last;
+	private int i;
 
-    protected void push(final int i) {
-        if (last == null) {
-            last = new Node(i);
-        } else {
-            last.setNext(new Node(i));
-            last.getNext().setPrev(last);
-            last = last.getNext();
-        }
-    }
-
-    protected boolean isEmpty() {
-        return last == null;
-    }
-
-    protected boolean isFull() {
-        return false;
-    }
-
-    protected int top() {
-        if (isEmpty()) {
-            return EMPTY;
-        }
-        return last.getValue();
-    }
-
-    protected int pop() {
-        if (isEmpty()) {
-            return EMPTY;
-        }
-        int ret = last.getValue();
-        last = last.getPrev();
-        return ret;
-    }
-
-	protected int getI() {
-		return i;
+	/**
+	 * Adds new element to list, creates new node
+	 * 
+	 * @param i value to put on list
+	 */
+	protected void push(final int i) {
+		if (last == null) {
+			last = new Node(i);
+		} else {
+			last.setNext(new Node(i));
+			last.getNext().setPrev(last);
+			last = last.getNext();
+		}
 	}
 
-	protected void setI(int i) {
-		this.i = i;
+	/**
+	 * Check if list is empty
+	 * 
+	 * @return true if list empty, else false
+	 */
+	protected boolean isEmpty() {
+		return last == null;
 	}
-	
+
+	/**
+	 * Check if list is full
+	 * 
+	 * @return true if list full, else false
+	 */
+	protected boolean isFull() {
+		return false;
+	}
+
+	/**
+	 * Returns value of last pushed value
+	 * 
+	 * @return value of last pushed value
+	 */
+	protected int top() {
+		if (isEmpty()) {
+			return EMPTY;
+		}
+		return last.getValue();
+	}
+
+	/**
+	 * Removes last pushed value
+	 * 
+	 * @return value of last pushed value
+	 */
+	protected int pop() {
+		if (isEmpty()) {
+			return EMPTY;
+		}
+		int ret = last.getValue();
+		last = last.getPrev();
+		return ret;
+	}
+
+	/**
+	 * Class representing node of list
+	 *
+	 */
 	private class Node {
 
-	    private final int value;
-	    private Node prev;
+		/**
+		 * Value stored in node
+		 */
+		private final int value;
+		/**
+		 * Reference to previous value
+		 */
+		private Node prev;
+		/**
+		 * Reference to next value
+		 */
 		private Node next;
 
-	    protected Node(final int i) {
-	        value = i;
-	    }
+		/**
+		 * Class constructor
+		 * @param i value to be stored in node
+		 */
+		protected Node(final int i) {
+			value = i;
+		}
 
+		/**
+		 * Returns value assigned to node
+		 * @return value of node
+		 */
 		public int getValue() {
 			return value;
 		}
 
+		/**
+		 * @return reference to previous node
+		 */
 		public Node getPrev() {
 			return prev;
 		}
 
+		/**
+		 * Sets reference to previous node 
+		 * @param prev previous node to be set
+		 */
 		public void setPrev(final Node prev) {
 			this.prev = prev;
 		}
 
+		/**
+		 * @return reference to next node
+		 */
 		public Node getNext() {
 			return next;
 		}
 
+		/**
+		 * Sets reference to next node 
+		 * @param next next node to be set
+		 */
 		public void setNext(final Node next) {
 			this.next = next;
 		}
