6c0b557731a0569cde3c2afc575054bb8bcbabf6
r.witczyk <r.witczyk@wp.pl>
2019-03-20 00:10:49 +0100

    3.3. 2. Dodana dokumentacja do klasy

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,24 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa HanoiRhymer rozszerza klase DefaultCountingOutRhymer
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
 	private int totalRejected = 0;
 
+	/**Metoda zwraca nam wartosc int totalRejected
+	 *
+	 * @return totalRejected
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * Sprawdza czy liczba wieksza od peekaboo()
+	 * @param in oznacza liczbe do sprawdzenia
+	 */
 	@Override
 	public void countIn(int in) {
 		if (!callCheck() && in > peekaboo())
