e10d522a035a91fa596546134fe65a91a66e9520
209412 <45198261+209412@users.noreply.github.com>
2019-03-19 16:20:20 -0700

    zad 3.1 pkt 12

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -37,5 +37,37 @@ public class IntLinkedList {
 		last = last.getPrev();
 		return ret;
 	}
+	
+}
+
+class Node {
+
+	private final int value;
+	private Node prev;
+	private Node next;
+
+	public Node(int i) {
+		value = i;
+	}
+
+	protected int getValue() {
+		return value;
+	}
+
+	protected Node getPrev() {
+		return prev;
+	}
+
+	protected void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	protected Node getNext() {
+		return next;
+	}
+
+	protected void setNext(Node next) {
+		this.next = next;
+	}
 
 }
