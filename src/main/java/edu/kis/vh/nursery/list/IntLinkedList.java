753b6e49851ab89bb8d43960759fbcf56f0d7741
struszczak.kamila <209472@edu.p.lodz.pl>
2019-03-19 22:40:45 +0100

    3.3.2 dodanie dokumentacji do klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,11 +1,19 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Lista przechowująca liczby całkowite.
+ */
 public class IntLinkedList {
 
     private final int EMPTY = -1;
     private Node last;
     private int i;
 
+    /**
+     * Umieszcza w liście element o przekazanej wartości.
+     *
+     * @param i wartość nowego elementu
+     */
     public void push(final int i) {
         if (last == null)
             last = new Node(i);
@@ -16,20 +24,36 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     * Sprawdza czy lista jest pusta.
+     * @return true jeśli lista jest pusta, false jeśli nie jest
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     * Sprawdza czy lista jest pełna.
+     * @return true jeśli lista jest pełna, false jeśli nie
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     * Zwraca wartośc ostatniego elementu na liście.
+     * @return ostatni element na liście lub -1 jeśli lista jest pusta
+     */
     public int top() {
         if (isEmpty())
             return EMPTY;
         return last.getValue();
     }
 
+    /**
+     * Zwraca ostatni element z listy i usuwa go.
+     * @return ostatni element na liście lub -1 jeśli lista jest pusta
+     */
     public int pop() {
         if (isEmpty())
             return EMPTY;
