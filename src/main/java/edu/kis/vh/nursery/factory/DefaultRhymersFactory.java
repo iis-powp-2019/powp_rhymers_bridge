cab959c816ad44d04d182f5db4e9e84ce541b7e1
Pelekon <pelekon@gmail.com>
2019-03-19 18:31:19 +0100

    Korekcja do punktu 3.3.2. Szkielet dokumentacji.

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,23 +4,42 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
+/**
+ * Class to create new instances of various available rhymers.
+ */
 public class DefaultRhymersFactory implements Rhymersfactory {
 
+    /**
+     * Method usable to create new instance of DefaultCountingOutRhymer.
+     * returns: new instance of DefaultCountingOutRhymer class.
+     */
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * Method usable to create new instance of DefaultCountingOutRhymer.
+     * returns: new instance of DefaultCountingOutRhymer class.
+     */
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * Method usable to create new instance of FIFORhymer.
+     * returns: new instance of FIFORhymer as object of type DefaultCountingOutRhymer.
+     */
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
 
+    /**
+     * Method usable to create new instance of HanoiRhymer.
+     * returns: new instance of HanoiRhymer as object of type DefaultCountingOutRhymer.
+     */
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
