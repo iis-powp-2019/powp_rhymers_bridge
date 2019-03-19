698cef8fd623e01688b95164c9c91decfb4a9231
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-19 11:50:49 +0100

    3.2.2. added unit test for all classes + attempted to improve existing tests

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -1,9 +1,25 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
+import edu.kis.vh.nursery.list.Node;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
+
+	@Test
+	public void testFactory() {
+		DefaultRhymersFactory rhymersFactory = new DefaultRhymersFactory();
+		Assert.assertEquals(DefaultCountingOutRhymer.class, rhymersFactory.getStandardRhymer().getClass());
+		Assert.assertEquals(DefaultCountingOutRhymer.class, rhymersFactory.getFalseRhymer().getClass());
+		Assert.assertEquals(FIFORhymer.class, rhymersFactory.getFIFORhymer().getClass());
+		Assert.assertEquals(HanoiRhymer.class, rhymersFactory.getHanoiRhymer().getClass());
+	}
+
+	@Test
+	public void testNode() {
+		Assert.assertEquals(666, new Node(666).getValue());
+	}
 
 	@Test
 	public void testCountIn() {
@@ -13,18 +29,25 @@ public class RhymersJUnitTest {
 
 		int result = rhymer.peekaboo();
 		Assert.assertEquals(testValue, result);
+
+		HanoiRhymer hanoiRhymer = new HanoiRhymer();
+		hanoiRhymer.countIn(5);
+		hanoiRhymer.countIn(7);
+		hanoiRhymer.countIn(9);
+		hanoiRhymer.countIn(3);
+		Assert.assertEquals(2, hanoiRhymer.reportRejected());
 	}
 
 	@Test
 	public void testCallCheck() {
 		DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
 		boolean result = rhymer.callCheck();
-		Assert.assertEquals(true, result);
+		Assert.assertTrue(result);
 
 		rhymer.countIn(888);
 
 		result = rhymer.callCheck();
-		Assert.assertEquals(false, result);
+		Assert.assertFalse(result);
 	}
 
 	@Test
@@ -33,12 +56,12 @@ public class RhymersJUnitTest {
 		final int STACK_CAPACITY = 12;
 		for (int i = 0; i < STACK_CAPACITY; i++) {
 			boolean result = rhymer.isFull();
-			Assert.assertEquals(false, result);
+			Assert.assertFalse(result);
 			rhymer.countIn(888);
 		}
 
 		boolean result = rhymer.isFull();
-		Assert.assertEquals(true, result);
+		Assert.assertTrue(result);
 	}
 
 	@Test
@@ -73,6 +96,15 @@ public class RhymersJUnitTest {
 		Assert.assertEquals(testValue, result);
 		result = rhymer.countOut();
 		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+
+		FIFORhymer fifoRhymer = new FIFORhymer();
+		fifoRhymer.countIn(1);
+		fifoRhymer.countIn(3);
+		fifoRhymer.countIn(3);
+		fifoRhymer.countIn(7);
+		result = fifoRhymer.countOut();
+		Assert.assertEquals(1, result);
+		Assert.assertEquals(2, fifoRhymer.getTotal());
 	}
 
 }
