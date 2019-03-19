320aa937faef0f9125762cc87d49facdd51ddf26
Piotr <amropz@gmail.com>
2019-03-19 01:04:19 +0100

    3.1.7

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,6 +8,10 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[CAPACITY];
 
+    public int getTotal() {
+        return total;
+    }
+
     private int total = EMPTY;
 
     public void countIn(int in) {
