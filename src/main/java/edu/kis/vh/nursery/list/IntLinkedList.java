224c83960598eb7620335eab1f873b8a02abda2c
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-19 11:53:41 +0100

    Revert "3.12. remove class which is not required to be public"
    This reverts commit 4808614a62f68afc8db34bc13ef5cc911648b283.

---
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -0,0 +1,44 @@
+package edu.kis.vh.nursery.list;
+
+//Alt-strzałka lewo - przejście do pliku znajdującego się na zakładce po lewej od obecnie edytowanego pliku
+//Alt-strzałka prawo - przejście do pliku znajdującego się na zakładce po prawej od obecnie edytowanego pliku
+
+public class IntLinkedList {
+
+    private static final int NO_ELEMENTS_LEFT = -1;
+    private Node last;
+    int i;
+
+    public void push(int i) {
+        if (last == null)
+            last = new Node(i);
+        else {
+            last.next = new Node(i);
+            last.next.prev = last;
+            last = last.next;
+        }
+    }
+
+    public boolean isEmpty() {
+        return last == null;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public int top() {
+        if (isEmpty())
+            return NO_ELEMENTS_LEFT;
+        return last.getValue();
+    }
+
+    public int pop() {
+        if (isEmpty())
+            return NO_ELEMENTS_LEFT;
+        int ret = last.getValue();
+        last = last.prev;
+        return ret;
+    }
+
+}
