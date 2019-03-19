01d4d834fd61dd3e9887505f67f10bc9d8cfb750
209412 <45198261+209412@users.noreply.github.com>
2019-03-19 16:52:08 -0700

    zad 3.3 pkt 2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,9 @@
 package edu.kis.vh.nursery;
 
+/**
+ * @author Jan
+ * 
+ */
 public class DefaultCountingOutRhymer {
 
 	private static final int BUFFER_SIZE = 12;
@@ -10,29 +14,47 @@ public class DefaultCountingOutRhymer {
 
 	private int total = TOTAL_START;
 
+	/**
+	 * @return total elements
+	 */
 	public int getTotal() {
 		return total;
 	}
 
+	/**
+	 * @param in insert in on top of stack
+	 */
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
 	}
 
+	/**
+	 * @return true if empty
+	 */
 	public boolean callCheck() {
 		return total == TOTAL_START;
 	}
 
+	/**
+	 * @return true if full
+	 */
 	public boolean isFull() {
 		return total == FULL_COUNT;
 	}
 
+	/**
+	 * @return number from top of the stack
+	 */
 	public int peekAboo() {
 		if (callCheck())
 			return TOTAL_START;
 		return numbers[total];
 	}
 
+	/**
+	 * @return number removed from top of the stack
+	 */
 	public int countOut() {
 		if (callCheck())
 			return TOTAL_START;
