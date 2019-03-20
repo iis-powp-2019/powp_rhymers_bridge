bf25acbe23683157246560ad6e4ad0d9dab39532
r.witczyk <r.witczyk@wp.pl>
2019-03-20 01:05:09 +0100

    12. POPRAWKA Prawidlowa wersja usunietej klasy

---
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -0,0 +1,75 @@
+package edu.kis.vh.nursery.list;
+
+public class IntLinkedList {
+
+    private Node last;
+    private int i;
+
+    public void push(int i) {
+        if (last == null)
+            last = new Node(i);
+        else {
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
+        }
+    }
+
+    public boolean isEmpty() {
+        return last == null;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public int top() {
+        if (isEmpty())
+            return -1;
+        return last.getValue();
+    }
+
+    public int pop() {
+        if (isEmpty())
+            return -1;
+        int ret = last.getValue();
+        last = last.getPrev();
+        return ret;
+    }
+
+}
+
+class Node {
+
+    private int value;
+    private Node prev;
+    private Node next;
+
+    public Node(int i) {
+        setValue(i);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
