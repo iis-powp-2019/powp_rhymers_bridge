a971b32dcced0a1ecfb326a0775d0705ab0ccf40
Monika Odartus <209410@edu.p.lodz.pl>
2019-03-20 18:34:43 +0100

    3.1 podpunkt 5 - wprowadzanie stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int STACK_CAPACITY = 12;
 
-	public int total = -1;
+	private static final int STACK_TOP = -1;
+
+	private int[] numbers = new int[STACK_CAPACITY];
+
+	public int total = STACK_TOP;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == STACK_TOP;
 	}
 		
 	public boolean isFull() {
-		return total == 11;
+		return total == STACK_CAPACITY - 1;
 	}
 		
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return STACK_TOP;
 		return numbers[total];
 	}
 			
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return STACK_TOP;
 		return numbers[total--];
 	}
 
