54083c64e1419adbaf62d1503eef80651ba5f140
banan <szy.grzelak@gmail.com>
2019-03-20 20:25:46 +0100

    3.1 podpunkt 5 Literały zastąpione stałymi

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,15 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int STACK_FULL_VALUE = 11;
 
-	public int total = -1;
+	private static final int STACK_EMPTY_VALUE = -1;
+
+	private static final int STACK_MAX_CAPACITY = 12;
+
+	private int[] numbers = new int[STACK_MAX_CAPACITY];
+
+	public int total = STACK_EMPTY_VALUE;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,22 +18,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == STACK_EMPTY_VALUE;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == STACK_FULL_VALUE;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return STACK_EMPTY_VALUE;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return STACK_EMPTY_VALUE;
 		return numbers[total--];
 	}
 
