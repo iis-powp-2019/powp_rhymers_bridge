c5b62b50424f7f658139ec0ab6d5b6d6c550f337
Monika Odartus <209410@edu.p.lodz.pl>
2019-03-20 21:13:11 +0100

    3.1 podpunkt 12 - przeniesienie klasy Node do klasy IntLinkedList + poprawka punktu 3.3.2

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -45,6 +45,67 @@ public class IntLinkedList {
 	public int getI() {
 		return i;
 	}
+	
+	
+	/**
+	 * Nested class in class IntLinkedList
+	 * @author monik
+	 */
+	class Node {
+
+		private final int value;
+		private Node prev;
+		private Node next;
+
+		/**
+		 * Constructor
+		 * @param i Variable assigned to the field value
+		 */
+		protected Node(int i) {
+			value = i;
+		}
+
+		/**
+		 * Returns node placed before this node in the stack
+		 * @return Node
+		 */
+		protected Node getPrev() {
+			return prev;
+		}
+
+		/**
+		 * Assigns node which is next in the stack to the field prev
+		 * @param prev
+		 */
+		protected void setPrev(Node prev) {
+			this.prev = prev;
+		}
+
+		/**
+		 * Returns node placed after this node in the stack
+		 * @return Node
+		 */
+		protected Node getNext() {
+			return next;
+		}
+
+		/**
+		 * Assigns node which is next in the stack to the field next
+		 * @param next 
+		 */
+		protected void setNext(Node next) {
+			this.next = next;
+		}
+
+		/**
+		 * Gets value of the node
+		 * @return int
+		 */
+		protected int getValue() {
+			return value;
+		}
+
+	}
 }
 
 //alt + <- przechodzi do poprzednio edytowanego pliku, za każdym razem o jeden w tył
