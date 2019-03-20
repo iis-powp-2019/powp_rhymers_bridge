8f92ded31c6e9f17cf5acb9159417aa34e27ff1c
Rahly <karol.oleksy97@gmail.com>
2019-03-20 19:16:19 +0100

    Wygenerowanie szkieletu dokumentacji

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -1,26 +1,46 @@
 package edu.kis.vh.nursery.factory;
 
+/**
+ * DefaultRhymersFactory is a class which contain four methods for DefaultCountingOutRhymer class
+ */
+
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
 public class DefaultRhymersFactory implements RhymersFactory {
 
+    /**
+     *
+     * @return new DefaultCountingOutRhymer for StandardRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     *
+     * @return new DefaultCountingOutRhymer for FalseRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     *
+     * @return new FIFORhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
 
+    /**
+     *
+     * @return new HanoiRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
