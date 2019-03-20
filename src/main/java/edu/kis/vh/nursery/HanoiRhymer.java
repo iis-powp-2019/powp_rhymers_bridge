95c418270c663b6159aac53248a3626ec76b94b9
Michal Kaczmarczyk <209344@edu.p.lodz.pl>
2019-03-20 16:49:34 +0100

    3.3.2 generated javadoc comments for HanoiRhymer class

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,22 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Class representing stack in which value can't be put if that value is higher than the current top of the stack
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+	/**
+	 * @return amount of rejected numbers
+	 */
 	public int reportRejected() {
 		return totalRejected;
 	}
 
+	/**
+	 * @param input value to be added to rhymer
+	 */
 	@Override
 	public void countIn(final int input) {
     	if (!callCheck() && input > peekaboo())
