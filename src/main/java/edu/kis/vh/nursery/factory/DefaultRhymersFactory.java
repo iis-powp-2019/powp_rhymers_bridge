7f108297ba28abade2074b205db907443437832a
209489 <lukasztracz195@gmail.com>
2019-03-20 21:24:31 +0100

    2. Comments and chars - create javaDocs

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -3,24 +3,36 @@ package edu.kis.vh.nursery.factory;
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
-
+/**
+* Class responsible for produce new objects for DefaultCountingOutRhymer
+*/
 public class DefaultRhymersFactory implements RhymersFactory {
 
+    /**
+    * Method responsible for create new object type DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     * Method responsible for create new object type DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
-
+    /**
+     * Method responsible for create new object type FIFORhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
-
+    /**
+     * Method responsible for create new object type HanoiRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
