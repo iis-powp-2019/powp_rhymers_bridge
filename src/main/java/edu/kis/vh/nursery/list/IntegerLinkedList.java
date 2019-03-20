77e8a07da792112a8ca9b93d18cc97d5166968e5
Mateusz Nowak <matenownow@gmail.com>
2019-03-20 22:51:10 +0100

    JavaDoc for IntegerLinkedList (part 3.3, task 2)

--- src/main/java/edu/kis/vh/nursery/list/IntegerLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntegerLinkedList.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Stores integers inside linked list, where each item has reference to previous and next one
+ */
 class IntegerLinkedList {
 
 	// EMPTY_STACK_VALUE (DefaultCountingOutRhymer) != EMPTY_LIST_VALUE (here)
@@ -7,6 +10,10 @@ class IntegerLinkedList {
 	
 	private Node lastNode;
 
+	/**
+	 * Add integer to a list
+	 * @param value Integer to be added
+	 */
 	private void push(int value) {
 		if (lastNode == null)
 			lastNode = new Node(value);
@@ -17,20 +24,38 @@ class IntegerLinkedList {
 		}
 	}
 
+	/**
+	 * Check whether list is empty
+	 * @return true if list is empty
+	 */
 	private boolean isEmpty() {
 		return lastNode == null;
 	}
 
+	/**
+	 * Check whether list is full
+	 * @return true if list is full
+	 */
 	private boolean isFull() {
 		return false;
 	}
 
+	/**
+	 * Returns firstly added integer, list will not be modified
+	 * @return Firstly added integer, or EMPTY_LIST_VALUE if list is empty
+	 * @see #pop()
+	 */
 	private int getTop() {
 		if (isEmpty())
 			return EMPTY_LIST_VALUE;
 		return lastNode.getValue();
 	}
 
+	/**
+	 * Removes and returns recently added integer
+	 * @return Recently added integer, or EMPTY_LIST_VALUE if list is empty
+	 * @see #getTop()
+	 */
 	private int pop() {
 		if (isEmpty())
 			return EMPTY_LIST_VALUE;
