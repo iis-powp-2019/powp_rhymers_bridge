996333afa92e59fb180c17f24421321954bb1b04
damon150196 <damian_golema@interia.eu>
2019-03-21 15:12:34 +0100

    Lab2 3.1.2 d
    Delegacja metod

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -17,6 +17,22 @@ public class DefaultCountingOutRhymer
 		this.ias = ias;
 	}
 	
+	public void countIn(int in) {
+		ias.countIn(in);
+	}
+	public boolean callCheck() {
+		return ias.callCheck();
+	}
+	public boolean isFull() {
+		return ias.isFull();
+	}
+	public int countOut() {
+		return ias.countOut();
+	}
+	public int peekaboo() {
+		return ias.peekaboo();
+	}
+	
 	
 
 }
