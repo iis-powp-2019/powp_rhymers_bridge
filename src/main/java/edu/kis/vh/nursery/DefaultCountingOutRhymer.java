ab10b02dc2e2abd71d08985e28b6686874a4b503
adamski126 <adamski126@gmail.com>
2019-03-21 14:41:21 +0100

    Bridge 3.1.2 *utworzono lokalny atrybut IntArrayStack *wygenerowano konsturktor *napisano konstruktor domyślny *wydelegowano realizację wszystkich operacji do obiektu IntArrayStack

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,53 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
+    IntArrayStack intArrayStack = new IntArrayStack();
 
+    public DefaultCountingOutRhymer(){
+
+    }
+
+    public DefaultCountingOutRhymer(IntArrayStack intArrayStack) {
+        this.intArrayStack = intArrayStack;
+    }
+
+    public static int getEmptyStackHeadIndex() {
+        return IntArrayStack.getEmptyStackHeadIndex();
+    }
+
+    public static int getFullStackHeadIndex() {
+        return IntArrayStack.getFullStackHeadIndex();
+    }
+
+    public static int getCAPACITY() {
+        return IntArrayStack.getCAPACITY();
+    }
+
+    public int getTotal() {
+        return intArrayStack.getTotal();
+    }
+
+    public void countIn(int in) {
+        intArrayStack.countIn(in);
+    }
+
+    public boolean callCheck() {
+        return intArrayStack.callCheck();
+    }
+
+    public boolean isFull() {
+        return intArrayStack.isFull();
+    }
+
+    public int peekaboo() {
+        return intArrayStack.peekaboo();
+    }
+
+    public int countOut() {
+        return intArrayStack.countOut();
+    }
+
+    public int[] getNumbers() {
+        return intArrayStack.getNumbers();
+    }
 }
