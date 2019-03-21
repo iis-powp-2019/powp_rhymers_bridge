e57437e7325a73b4ab8d842e17652980b18f233c
Lukkario <michallukianski.0@gmail.com>
2019-03-21 14:29:15 +0100

    Bridge 3.1.1 Added IntArrayStack with implementation.

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,63 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+    private static final int EMPTY = -1;
+    private static final int NUMBERS_SIZE = 12;
+    private static final int FULL = 11;
+    private int[] numbers = new int[getNumbersSize()];
+
+    private int total = getEMPTY();
+
+    public static int getEMPTY() {
+        return EMPTY;
+    }
+
+    public static int getNumbersSize() {
+        return NUMBERS_SIZE;
+    }
+
+    public static int getFULL() {
+        return FULL;
+    }
+
+    public void countIn(int in) {
+        if (!isFull())
+            getNumbers()[++total] = in;
+    }
+
+    boolean callCheck() {
+        return total == getEMPTY();
+    }
+
+    boolean isFull() {
+        return total == getFULL();
+    }
+
+    int peekaboo() {
+        if (callCheck())
+            return getEMPTY();
+        return getNumbers()[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return getEMPTY();
+        return getNumbers()[total--];
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+}
