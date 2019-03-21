b9cb49f2aab59ad3d5b03e30a17266f3ad2febe4
Piotrek <piotr.marczewski95@gmail.com>
2019-03-21 14:50:44 +0100

    Skopiowanie implementacji z default... class do IntArrayStack

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,41 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+	private static final int error = -1;
+
+	private static final int maxArraySize = 12;
+
+	private int[] numbers = new int[maxArraySize];
+
+	private int total = error;
+
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+	
+	public boolean callCheck() {
+		return total == error;
+	}
+	
+	public boolean isFull() {
+		return total == 11;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	
+	protected int peekaboo() {
+		if (callCheck())
+			return error;
+		return numbers[total];
+	}
+	
+	public int countOut() {
+		if (callCheck())
+			return error;
+		return numbers[total--];
+	}
+}
