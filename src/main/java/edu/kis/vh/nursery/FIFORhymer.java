92db495b5c950e3f011ae26cb9000c2b2c1a77db
adamski126 <adamski126@gmail.com>
2019-03-26 21:53:22 +0100

    Bridge 3.1.11 *Zoptymalizowano klasę FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,10 +1,11 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.storage.IntArrayStack;
 import edu.kis.vh.nursery.storage.IntLinkedList;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private final DefaultCountingOutRhymer TEMP = new DefaultCountingOutRhymer();
+    private final IntLinkedList TEMP = new IntLinkedList();
 
     public FIFORhymer() {
         super();
@@ -18,17 +19,17 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
     public int countOut() {
 
         while (!callCheck())
-            TEMP.countIn(super.countOut());
+            TEMP.push(super.countOut());
 
-        int ret = TEMP.countOut();
+        int ret = TEMP.pop();
 
-        while (!TEMP.callCheck())
-            countIn(TEMP.countOut());
+        while (!TEMP.isEmpty())
+            countIn(TEMP.pop());
 
         return ret;
     }
 
-    public DefaultCountingOutRhymer getTemp() {
+    public IntLinkedList getTemp() {
         return TEMP;
     }
 }
