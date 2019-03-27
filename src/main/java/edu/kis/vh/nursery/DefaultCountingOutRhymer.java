b849fe02f593e7910b82107761e4ae1c528a2aae
r.witczyk <r.witczyk@wp.pl>
2019-03-27 15:08:35 +0100

    3.1.2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,6 +1,37 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
+private IntArrayStack intArrayStack;
 
+	public DefaultCountingOutRhymer(IntArrayStack intArrayStack) {
+		this.intArrayStack = intArrayStack;
+	}
 
+	public DefaultCountingOutRhymer() {
+		this.intArrayStack = new IntArrayStack();
+	}
+
+	public int getTotal() {
+		return intArrayStack.getTotal();
+	}
+
+	public void countIn(int in) {
+		intArrayStack.countIn(in);
+	}
+
+	public boolean callCheck() {
+		return intArrayStack.callCheck();
+	}
+
+	public boolean isFull() {
+		return intArrayStack.isFull();
+	}
+
+	public int peekaboo() {
+		return intArrayStack.peekaboo();
+	}
+
+	public int countOut() {
+		return intArrayStack.countOut();
+	}
 }
