9e5e427b69972a1b8a53876076d2157089cb6c3f
ostrowski.kamil <209418@edu.p.lodz.pl>
2019-03-27 14:39:00 +0100

    3.1.1 Skopiowanie implementacji stosu z DefaultCountingOutRhymer do IntArrayStack

--- src/main/java/edu/kis/vh/nursery/factory/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/factory/IntArrayStack.java
@@ -1,6 +1,11 @@
 package edu.kis.vh.nursery.factory;
 
+
+/**
+ * Class which counting down, which is working like a stack.
+ */
 public class IntArrayStack {
+
     private static final int MIN = -1;
     private static final int MAX = 11;
     private static final int MAX_SIZE_NUMBERS = 12;
@@ -8,29 +13,47 @@ public class IntArrayStack {
 
     private int total = -1;
 
+    /**
+     * @return actual number elements of stacks;
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * @param IN number which is using to fill the stack.
+     */
     public void countIn(final int IN) {
         if (!isFull())
             numbers[++total] = IN;
     }
 
+    /**
+     * @return return true, if stack is empty and false, if it's not.
+     */
     public boolean callCheck() {
         return total == MIN;
     }
 
+    /**
+     * @return return true, if stack is full and false, if stack is not.
+     */
     public boolean isFull() {
         return total == MAX;
     }
 
+    /**
+     * @return return -1, if stack is empty or actual element, if its not.
+     */
     protected int peekaboo() {
         if (callCheck())
             return MIN;
         return numbers[total];
     }
 
+    /**
+     * @return return -1, if stack is empty or under the top element, if its not.
+     */
     public int countOut() {
         if (callCheck())
             return MIN;
