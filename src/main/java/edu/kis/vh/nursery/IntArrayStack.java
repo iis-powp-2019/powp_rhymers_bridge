3b2ad73fb0169ffad04bd12368d904111530be5e
Patryk Gitner <209317@edu.p.lodz.pl>
2019-03-27 14:31:51 +0100

    3.1.1 - utworzenie klasy IntArrayStack oraz przeniesienie implementacji DefaultCountingOutRhymer do nowej klasy

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,43 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    private static final int STACK_CAPACITY = 12;
+    private static final int EMPTY_STACK_VALUE = -1;
+
+    private int[] numbers = new int[STACK_CAPACITY];
+    private int total = EMPTY_STACK_VALUE;
+
+    public void countIn(int in) {
+        if (!isFull()) {
+            numbers[++total] = in;
+        }
+    }
+
+    boolean callCheck() {
+        return total == EMPTY_STACK_VALUE;
+    }
+
+    boolean isFull() {
+        return total == STACK_CAPACITY - 1;
+    }
+
+    int actualNumber() {
+        if (callCheck()) {
+            return EMPTY_STACK_VALUE;
+        }
+
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck()) {
+            return EMPTY_STACK_VALUE;
+        }
+        return numbers[total--];
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+}
