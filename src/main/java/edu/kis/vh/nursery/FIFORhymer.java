0117e84c38e3ea65b5de40c6a2a87fa31b332eae
Jędrek <j-szelag@gazeta.pl>
2019-03-27 23:53:17 +0100

    3.1.10 - constructors generated

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,16 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stackImplementation.StackInterface;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+    public FIFORhymer() { super();}
+
+    public FIFORhymer(StackInterface stackInterface) {
+        super(stackInterface);
+    }
 
     @Override
     public int countOut() {
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,20 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stackImplementation.StackInterface;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    public HanoiRhymer() { super(); }
+
+    public HanoiRhymer(StackInterface stackInterface) {
+        super(stackInterface);
+    }
+
     public int reportRejected() {
         return totalRejected;
     }
-
 
     @Override
     public void countIn(final int in) {
