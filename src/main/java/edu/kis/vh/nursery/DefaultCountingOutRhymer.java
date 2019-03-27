09263d47af9e747368a88aff8102d182b339dc25
r.witczyk <r.witczyk@wp.pl>
2019-03-27 14:41:54 +0100

    3.1.1

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,41 +2,5 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private static final int DEFAULT = 12;
-
-	private static final int STACK_CAPACITY = -1;
-
-	private final int[] numbers = new int[DEFAULT];
-
-	public int getTotal() {
-		return total;
-	}
-
-	private int total = STACK_CAPACITY;
-
-	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
-	}
-
-		public boolean callCheck() {
-			return total == STACK_CAPACITY;
-		}
-		
-			public boolean isFull() {
-				return total == 11;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return STACK_CAPACITY;
-					return numbers[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return STACK_CAPACITY;
-						return numbers[total--];
-					}
 
 }
