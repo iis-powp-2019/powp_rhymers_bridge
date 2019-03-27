1c5e3b48eda14fd343c4ad2239808ed25d1c287a
Lukkario <michallukianski.0@gmail.com>
2019-03-27 14:17:58 +0100

    Bridge 3.1.12 Added Rhymers factory for ArrayStack and LinkedList

---
+++ src/main/java/edu/kis/vh/nursery/factory/ArrayRhymersFactory.java
@@ -0,0 +1,31 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.storage.IntArrayStack;
+import edu.kis.vh.nursery.storage.IntLinkedList;
+
+public class ArrayRhymersFactory implements Rhymersfactory{
+
+        @Override
+        public DefaultCountingOutRhymer getStandardRhymer() {
+            return new DefaultCountingOutRhymer(new IntArrayStack());
+        }
+
+        @Override
+        public DefaultCountingOutRhymer getFalseRhymer() {
+            return new DefaultCountingOutRhymer(new IntLinkedList());
+        }
+
+        @Override
+        public DefaultCountingOutRhymer getFIFORhymer() {
+            return new FIFORhymer(new IntArrayStack());
+        }
+
+        @Override
+        public DefaultCountingOutRhymer getHanoiRhymer() {
+            return new HanoiRhymer(new IntArrayStack());
+        }
+
+}
---
+++ src/main/java/edu/kis/vh/nursery/factory/ListRhymersFactory.java
@@ -0,0 +1,30 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.storage.IntArrayStack;
+import edu.kis.vh.nursery.storage.IntLinkedList;
+
+public class ListRhymersFactory implements Rhymersfactory {
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+
+}
