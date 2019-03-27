87e7f18c6f3084d4a5a904718fd1ada9f6c5887c
Kamil Jurek <informator017@wp.pl>
2019-03-27 14:32:25 +0100

    3.1.1 Copy content of DefaultCountingOutRhymer to new class IntArrayStack.

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,45 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    private static final int STACK_CAPACITY = 12;
+    private static final int EMPTY_STACK_SIZE = -1;
+
+    private int[] numbers = new int[STACK_CAPACITY];
+    private int total = EMPTY_STACK_SIZE;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    protected void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    protected boolean callCheck() {
+        return total == -1;
+    }
+
+
+    public boolean isFull() {
+        return total == 11;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return -1;
+        return numbers[total];
+    }
+
+    protected int countOut() {
+        if (callCheck())
+            return -1;
+        return numbers[total--];
+    }
+
+}
