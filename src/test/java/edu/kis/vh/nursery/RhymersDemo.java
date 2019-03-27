6fea7d2b023e7e115f2d0ec559b8aeaed7c3408b
Lukkario <michallukianski.0@gmail.com>
2019-03-27 14:29:17 +0100

    Bridge 3.1.13 Added all factories.

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,14 +1,27 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.factory.ArrayRhymersFactory;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
+import edu.kis.vh.nursery.factory.ListRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class RhymersDemo {
 
 	public static void main(String[] args) {
-		Rhymersfactory factory = new DefaultRhymersFactory();
+		List<Rhymersfactory> factories = new ArrayList<>();
+		factories.add(new DefaultRhymersFactory());
+		factories.add(new ListRhymersFactory());
+		factories.add(new ArrayRhymersFactory());
 
-		DefaultCountingOutRhymer[] rhymers = testRhymers(factory);
+		factories.forEach(RhymersDemo::testRhymers);
+	}
+
+	private static void testRhymers(Rhymersfactory factory) {
+		DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
+				factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
 		for (int i = 1; i < 15; i++)
 			for (int j = 0; j < 3; j++)
@@ -17,21 +30,15 @@ class RhymersDemo {
 		java.util.Random rn = new java.util.Random();
 		for (int i = 1; i < 15; i++)
 			rhymers[3].countIn(rn.nextInt(20));
-		
+
 		for (int i = 0; i < rhymers.length; i++) {
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
-		
+
 		System.out.println("total rejected is "
 				+ ((HanoiRhymer) rhymers[3]).reportRejected());
-		
-	}
-
-	private static DefaultCountingOutRhymer[] testRhymers(Rhymersfactory factory) {
-		return new DefaultCountingOutRhymer[]{ factory.getStandardRhymer(), factory.getFalseRhymer(),
-					factory.getFIFORhymer(), factory.getHanoiRhymer()};
 	}
 
 }
