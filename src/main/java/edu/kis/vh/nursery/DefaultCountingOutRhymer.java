0d11265ee5698d74cddd7e4d495311dd0b1924f8
ostrowski.kamil <209418@edu.p.lodz.pl>
2019-03-27 14:36:48 +0100

    3.1.1 Skopiowanie implementacji stosu z DefaultCountingOutRhymer do IntArrayStack

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,40 +2,5 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int MIN = -1;
-    private static final int MAX = 11;
-    private static final int MAX_SIZE_NUMBERS = 12;
-    private int[] numbers = new int[MAX_SIZE_NUMBERS];
-
-    private int total = -1;
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void countIn(final int IN) {
-        if (!isFull())
-            numbers[++total] = IN;
-    }
-
-    public boolean callCheck() {
-        return total == MIN;
-    }
-
-    public boolean isFull() {
-        return total == MAX;
-    }
-
-    protected int peekaboo() {
-        if (callCheck())
-            return MIN;
-        return numbers[total];
-    }
-
-    public int countOut() {
-        if (callCheck())
-            return MIN;
-        return numbers[total--];
-    }
 
 }
