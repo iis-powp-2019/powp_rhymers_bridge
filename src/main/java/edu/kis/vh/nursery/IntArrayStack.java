a932887000241f5070a09b00fa608b946875876e
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-03-27 14:42:56 +0100

    lab_2 IntArrayStack added (3.1.1)

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,52 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    private static final int STACK_CAPACITY = 12;
+    private static final int MIN_TOTAL_VALUE = -1;
+    private int[] numbers = new int[getStackCapacity()];
+
+    public static int getStackCapacity() {
+        return STACK_CAPACITY;
+    }
+
+    public static int getMinTotalValue() {
+        return MIN_TOTAL_VALUE;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    private int total = getMinTotalValue();
+
+    public void countIn(int in) {
+        if (!isFull())
+            getNumbers()[++total] = in;
+    }
+
+    boolean callCheck() {
+        return total == getMinTotalValue();
+    }
+
+    boolean isFull() {
+        return total == 11;
+    }
+
+    int peekaboo() {
+        if (callCheck())
+            return -1;
+        return getNumbers()[total];
+    }
+
+    int countOut() {
+        if (callCheck())
+            return -1;
+        return getNumbers()[total--];
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+}
