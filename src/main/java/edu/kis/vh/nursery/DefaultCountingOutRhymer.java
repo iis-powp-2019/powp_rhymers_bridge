d0e9261cf4f01a18f3cd698276678c8130ed0d7a
KovalykVolodymyr <kovalykvovapl@gmail.com>
2019-03-27 14:46:53 +0100

    3.1.2 write new code In DefaultCountingOutRhymer using class IntArrayStack

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,45 +2,41 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private static final int STACK_MAX_SIZE = 12;
-    private static final int NEGATIVE_NUMBER = -1;
-    private static final int FULL_INDEX = 11;
-    private static final int INDEX_EMPTY = -1;
-    private static final int NAMBER_OUT = -1;
-    private static final int NAMBER_PIKABO = -1;
-    private int[] numbers = new int[STACK_MAX_SIZE];
+    private IntArrayStack stack;
 
-    public int getTotal() {
-        return total;
+
+    public DefaultCountingOutRhymer(IntArrayStack stack) {
+        this.stack = stack;
     }
 
 
-
-    private int total = INDEX_EMPTY;
+    public int getTotal() {
+        return stack.getTotal();
+    }
 
     public void countIn(int in) {
-        if (!isFull())
-            numbers[++total] = in;
+        stack.countIn(in);
     }
 
     public boolean callCheck() {
-        return total == NEGATIVE_NUMBER;
+        return stack.callCheck();
     }
 
     public boolean isFull() {
-        return total == FULL_INDEX;
+        return stack.isFull();
     }
 
     protected int peekaboo() {
-        if (callCheck())
-            return NAMBER_PIKABO;
-        return numbers[total];
+        return stack.peekaboo();
     }
 
     public int countOut() {
-        if (callCheck())
-            return NAMBER_OUT;
-        return numbers[total--];
+        return stack.countOut();
     }
+
+    public DefaultCountingOutRhymer() {
+        this.stack = new IntArrayStack();
+    }
+
 
 }
