4e4656b6d324999bc68bcbcd7ac66b73ca567e42
Pelekon <pelekon@gmail.com>
2019-03-27 14:36:51 +0100

    Wykonanie punktu 1. Klasa IntArrayStack.

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,40 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack
+{
+    private static final int STARTING_INT = -1;
+    private static final int MAX_TAB_SIZE = 12;
+    private static final int FULL_INT = 11;
+    private int[] numbers = new int[MAX_TAB_SIZE];
+
+    public int getTotal() {
+        return total;
+    }
+
+    private int total = STARTING_INT;
+
+    public void countIn(final int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    boolean callCheck() {
+        return total == STARTING_INT;
+    }
+
+    boolean isFull() {
+        return total == FULL_INT;
+    }
+
+    int peekaboo() {
+        if (callCheck())
+            return STARTING_INT;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return STARTING_INT;
+        return numbers[total--];
+    }
+}
