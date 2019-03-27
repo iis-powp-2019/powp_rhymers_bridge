1ae6a3410f4ae53fa08e8332fe49f09b7b12fb76
209278 <209278@edu.p.lodz.pl>
2019-03-27 14:28:40 +0100

    3.1.1

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,43 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    public static final int DEFAULT = -1;
+    public static final int MAX_NUMBER_OF_ELEMENTS = 12;
+
+    private final int[] numbers = new int[MAX_NUMBER_OF_ELEMENTS];
+
+    private int total = DEFAULT;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == DEFAULT;
+    }
+
+    public boolean isFull() {
+        return total == 11;
+    }
+
+    protected int peekaboo() {
+        if (callCheck()) {
+            return DEFAULT;
+        }
+        return numbers[getTotal()];
+    }
+
+    public int countOut() {
+        if (callCheck()) {
+            return DEFAULT;
+        }
+        return numbers[total--];
+    }
+
+}
