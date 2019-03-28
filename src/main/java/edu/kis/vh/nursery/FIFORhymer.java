2adb6af2a1dbf8a77d14045e9b8bc38ee70c67c3
Piotrek <piotr.marczewski95@gmail.com>
2019-03-28 17:30:04 +0100

    ver. 3.1.10 generacja superclass constructors

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,5 +1,7 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.newStackImplementaion.IntLinkedList;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
 	private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
@@ -17,6 +19,14 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
 			countIn(temp.countOut());
 
 		return ret;
+	}
+
+	public FIFORhymer() {
+		super();
+	}
+
+	public FIFORhymer(IntLinkedList linkedList) {
+		super(linkedList);
 	}
 }
 // alt + " strzałka " przełączanie kart 
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,6 +1,16 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.newStackImplementaion.IntLinkedList;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
+
+	public HanoiRhymer() {
+		super();
+	}
+
+	public HanoiRhymer(IntLinkedList linkedList) {
+		super(linkedList);
+	}
 
 	private int totalRejected = 0;
 	
