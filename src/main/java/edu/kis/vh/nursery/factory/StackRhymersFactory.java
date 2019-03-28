b3729b751254575bba188f72c47c389e46b9c1a5
209489 <lukasztracz195@gmail.com>
2019-03-28 20:46:55 +0100

    3.1.12

---
+++ src/main/java/edu/kis/vh/nursery/factory/StackRhymersFactory.java
@@ -0,0 +1,28 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.collections.IntArrayStack;
+
+public class StackRhymersFactory implements RhymersFactory {
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer();
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntArrayStack());
+    }
+}
