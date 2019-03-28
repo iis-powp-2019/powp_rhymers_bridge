2a71254560e980b952e89996c0650a50674f4996
Piotrek <piotr.marczewski95@gmail.com>
2019-03-28 14:42:19 +0100

    test

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,7 +1,44 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-	private IntArrayStack arrayStack;
+	
+	private static final int error = -1;
+
+	private static final int maxArraySize = 12;
+
+	private int[] numbers = new int[maxArraySize];
+
+	private int total = error;
+
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+	
+	public boolean callCheck() {
+		return total == error;
+	}
+	
+	public boolean isFull() {
+		return total == 11;
+	}
+	
+	public int getTotal() {
+		return total;
+	}
+	
+	protected int peekaboo() {
+		if (callCheck())
+			return error;
+		return numbers[total];
+	}
+	
+	public int countOut() {
+		if (callCheck())
+			return error;
+		return numbers[total--];
+	}
+	/*private IntArrayStack arrayStack;
 
 	public DefaultCountingOutRhymer(IntArrayStack arrayStack) {
 		super();
@@ -31,4 +68,12 @@ public class DefaultCountingOutRhymer {
 	public int countOut() {
 		return arrayStack.countOut();
 	}
+	
+    public int peekaboo() {
+        return arrayStack.peekaboo();
+    }*/
+    
+    
 }
+
+
