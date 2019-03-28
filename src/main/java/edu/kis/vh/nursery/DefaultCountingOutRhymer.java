538f9cb3073eb1b4bd119ab6f018642172ede643
Dominik Rak <rakdominikrak@gmail.com>
2019-03-28 15:32:23 +0100

    3.1.7 zmiana nazwy pakietu

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.list.IntLinkedList;
-import edu.kis.vh.nursery.list.Stack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
+import edu.kis.vh.nursery.stack.Stack;
 
 public class DefaultCountingOutRhymer {
 
--- src/main/java/edu/kis/vh/nursery/stack/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/stack/IntLinkedList.java
@@ -1,4 +1,4 @@
-package edu.kis.vh.nursery.list;
+package edu.kis.vh.nursery.stack;
 
 /**
  * @author Dominik Class implementing int doulby linked list
