1e7beeef3fa6f04dc87948467fdbe909cdfe1388
209360 <209360@10.0.75.1>
2019-03-28 15:29:27 +0100

    3.1.6 Move IntArrayStack to package edu.kis.vh.nursery.list

--- src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -1,4 +1,6 @@
-package edu.kis.vh.nursery;
+package edu.kis.vh.nursery.list;
+
+import edu.kis.vh.nursery.Collections;
 
 /**
  * @author Maciek Implementing a stack of integers with pointer on last added element.
