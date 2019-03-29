92d49379c6f03eb626abf235a1e699294e1d78c8
Lucat0n <lukasz662@gmail.com>
2019-03-29 16:58:01 +0100

    3.1.10 Generacja konstruktorów dla wyliczanek

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,22 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.storage.IntStorage;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private DefaultCountingOutRhymer temp;
+
+    public FIFORhymer(IntStorage intStorage, DefaultCountingOutRhymer temp)
+    {
+        super(intStorage);
+        this.temp = temp;
+    }
+
+    public FIFORhymer()
+    {
+        super();
+        this.temp = new DefaultCountingOutRhymer();
+    }
 
     @Override
     int countOut() {
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,11 +1,22 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.storage.IntStorage;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    private int totalRejected = 0;
+    private int totalRejected;
 
     int reportRejected() {
         return totalRejected;
+    }
+
+    public HanoiRhymer(IntStorage intStorage, int totalRejected) {
+        super(intStorage);
+        this.totalRejected = totalRejected;
+    }
+
+    public HanoiRhymer(){
+        this.totalRejected = 0;
     }
 
     @Override
