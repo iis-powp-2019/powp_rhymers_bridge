f1834d81c66e171dcfbfb2b4473d4b5bc35c18d8
Dominik <rakdominikrak@gmail.com>
2019-03-29 09:11:35 +0100

    3.1.10 generacja konstruktorów z nadklasy

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,6 +1,18 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.Stack;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
+
+	public FIFORhymer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FIFORhymer(Stack stack) {
+		super(stack);
+		// TODO Auto-generated constructor stub
+	}
 
 	private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,7 +1,17 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.Stack;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+	public HanoiRhymer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public HanoiRhymer(Stack stack) {
+		super(stack);
+		// TODO Auto-generated constructor stub
+	}
 	private int totalRejected = 0;
 
 	public int reportRejected() {
