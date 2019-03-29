cd96cd8e7e2ec23aa71ee1a33eac58c0036ae46c
Dominik <rakdominikrak@gmail.com>
2019-03-29 09:36:10 +0100

    bridge 3.1.12 dodanie fabryb

---
+++ src/main/java/edu/kis/vh/nursery/factory/FIFORhymerFactory.java
@@ -0,0 +1,32 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+
+public class FIFORhymerFactory implements RhymersFactory {
+
+	@Override
+	public DefaultCountingOutRhymer getStandardRhymer() {
+		return new FIFORhymer();
+
+	}
+
+	@Override
+	public DefaultCountingOutRhymer getFalseRhymer() {
+		return new HanoiRhymer();
+ 
+	}
+
+	@Override
+	public DefaultCountingOutRhymer getFIFORhymer() {
+		return new FIFORhymer();
+
+	}
+
+	@Override
+	public DefaultCountingOutRhymer getHanoiRhymer() {
+		return new HanoiRhymer();
+	}
+
+}
---
+++ src/main/java/edu/kis/vh/nursery/factory/HanoiRhymerFactory.java
@@ -0,0 +1,30 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+
+public class HanoiRhymerFactory implements RhymersFactory {
+
+	@Override
+	public DefaultCountingOutRhymer getStandardRhymer() {
+		return new HanoiRhymer();
+	}
+
+	@Override
+	public DefaultCountingOutRhymer getFalseRhymer() {
+		return new FIFORhymer();
+	}
+
+	@Override
+	public DefaultCountingOutRhymer getFIFORhymer() {
+		return new FIFORhymer();
+	}
+
+	@Override
+	public DefaultCountingOutRhymer getHanoiRhymer() {
+		return new HanoiRhymer();
+
+	}
+
+}
