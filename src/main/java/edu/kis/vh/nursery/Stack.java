0b645f5eb96427e19c7df588c7e9686f17d747ca
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-03-31 13:47:51 +0200

    3.1.8 Przniesienie stalej EMPTY_STACK_VALUE do interfejsu Stack

--- src/main/java/edu/kis/vh/nursery/Stack.java
+++ src/main/java/edu/kis/vh/nursery/Stack.java
@@ -2,6 +2,8 @@ package edu.kis.vh.nursery;
 
 public interface Stack {
 
+    int EMPTY_STACK_VALUE = -1;
+
     int getTotal();
 
     void countIn(int in);
--- src/main/java/edu/kis/vh/nursery/list/datastructures/IntArrayStack.java
+++ src/main/java/edu/kis/vh/nursery/list/datastructures/IntArrayStack.java
@@ -4,15 +4,15 @@ import edu.kis.vh.nursery.Stack;
 
 public class IntArrayStack implements Stack {
 
-    private static final int TOTAL_VALUE = -1;
-
     private static final int MAX_SIZE = 12;
 
     private final int[] numbers = new int[MAX_SIZE];
 
-    private int total = TOTAL_VALUE;
+    private int total = Stack.EMPTY_STACK_VALUE;
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     *
      * @see edu.kis.vh.nursery.Stack#getTotal()
      */
     @Override
@@ -20,7 +20,9 @@ public class IntArrayStack implements Stack {
         return total;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     *
      * @see edu.kis.vh.nursery.Stack#countIn(int)
      */
     @Override
@@ -30,15 +32,19 @@ public class IntArrayStack implements Stack {
         }
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     *
      * @see edu.kis.vh.nursery.Stack#callCheck()
      */
     @Override
     public boolean callCheck() {
-        return total == TOTAL_VALUE;
+        return total == Stack.EMPTY_STACK_VALUE;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     *
      * @see edu.kis.vh.nursery.Stack#isFull()
      */
     @Override
@@ -46,24 +52,28 @@ public class IntArrayStack implements Stack {
         return total == 11;
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     *
      * @see edu.kis.vh.nursery.Stack#peekaboo()
      */
     @Override
     public int peekaboo() {
         if (callCheck()) {
-            return TOTAL_VALUE;
+            return Stack.EMPTY_STACK_VALUE;
         }
         return numbers[total];
     }
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     *
      * @see edu.kis.vh.nursery.Stack#countOut()
      */
     @Override
     public int countOut() {
         if (callCheck()) {
-            return TOTAL_VALUE;
+            return Stack.EMPTY_STACK_VALUE;
         }
         return numbers[total--];
     }
