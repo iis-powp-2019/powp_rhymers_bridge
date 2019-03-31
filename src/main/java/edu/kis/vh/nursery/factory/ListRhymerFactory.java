b29628e0d69c15a007061a651c9fabf079e2a5ac
Wiktor <wiktor756@gmail.com>
2019-03-31 18:16:59 +0200

    bridge 3.1.12 Dodanie Array oraz List Factory

---
+++ src/main/java/edu/kis/vh/nursery/factory/ListRhymerFactory.java
@@ -0,0 +1,29 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
+public class ListRhymerFactory implements Rhymersfactory {
+    @Override
+    public DefaultCountingOutRhymer GetStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer GetFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer GetFIFORhymer() {
+        return new FIFORhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer GetHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+}
