514b0fcfbaf4c089c65d1eb485d66c35714fd761
KovalykVolodymyr <kovalykvovapl@gmail.com>
2019-04-01 18:50:08 +0200

    3.1.12   Add new class ListStackRhymesFactory and TableStackRhymesFactory

---
+++ src/main/java/edu/kis/vh/nursery/factory/TabelStackRyhmersFactory.java
@@ -0,0 +1,29 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.list.IntLinkedList;
+
+public class TabelStackRyhmersFactory implements Rhymersfactory{
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntArrayStack());
+    }
+}
