edc20ed6b62e644aca6c1e3cf7ecf78b53909832
Igor J <igij@o2.pl>
2019-04-01 15:43:09 +0200

    Lab 1 - 3.3.2

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,23 +8,43 @@ public class DefaultCountingOutRhymer {
     private final int[] stack = new int[STACK_MAX_CAPACITY];
     private int currIndex = EMPTY_INDEX_VALUE;
 
+    /**
+     * Get index which indicative top of the stack
+     * @return this index
+     */
     public int getCurrIndex() {
         return currIndex;
     }
 
+    /**
+     * Add integer value to the stack
+     * @param in the value that will be added to the stack
+     */
     public void countIn(int in) {
         if (!isFull())
             stack[++currIndex] = in;
     }
 
+    /**
+     *
+     * @return true, if the stack is empty, otherwise false
+     */
     public boolean callCheck() {
         return currIndex == EMPTY_INDEX_VALUE;
     }
 
+    /**
+     *
+     * @return true, if the stack is full, otherwise false
+     */
     public boolean isFull() {
         return currIndex == (STACK_MAX_CAPACITY - 1);
     }
 
+    /**
+     * Get value from the top of the stack WITHOUT decrease stack pointer
+     * @return EMPTY_INDEX_VALUE if stack is empty, otherwise value from the top of stack
+     */
     protected int peekaboo() {
         if (callCheck())
             return EMPTY_INDEX_VALUE;
@@ -32,6 +52,10 @@ public class DefaultCountingOutRhymer {
         return stack[currIndex];
     }
 
+    /**
+     * Get value from the top of the stack WITH decrease stack pointer
+     * @return EMPTY_INDEX_VALUE if stack is empty, otherwise value from the top of stack
+     */
     public int countOut() {
         if (callCheck())
             return EMPTY_INDEX_VALUE;
