86df6f9b676d54c6f6e2875c448692415de717d2
Igor Jarek <igij@o2.pl>
2019-04-01 22:12:01 +0200

    Lab 2 - 3.1.11

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,14 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stackImpl.IntLinkedList;
+import edu.kis.vh.nursery.stackImpl.IntStack;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private final DefaultCountingOutRhymer defaultRhymer = new DefaultCountingOutRhymer();
-
+    // Według mnie najlepsza będzie implementacja oparta na liście ponieważ :
+    // 1. Może przechowywać nieograniczoną ilość elementów
+    // 2. Wstawianie jak i usuwanie z początku listy jest wykonywanie w stałym czasie
+    private final IntStack defaultRhymer = new IntLinkedList();
     public FIFORhymer() {
         super();
     }
