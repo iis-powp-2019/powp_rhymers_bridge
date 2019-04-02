b075396ccc79f0518466644715332104d67255eb
marax97 <pa-wo97@o2.pl>
2019-04-02 17:05:02 +0200

    3.1.13 i 3.1.15 - Dodanie testow i sprawdzenie czy przechodza

--- src/main/java/edu/kis/vh/nursery/FIFORhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymersFactory.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
-public class FIFORhymerFactory implements Rhymersfactory {
+public class FIFORhymersFactory implements Rhymersfactory {
 
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
