6158e060ad9f8ea4a4314a8665b26cb543afc5fd
Piotr Zając <p.zajac@samorzad.p.lodz.pl>
2019-04-02 20:01:52 +0200

    Bridge 3.1.5 and 3.1.6

---
+++ src/main/java/edu/kis/vh/nursery/list/IntArrayStack.java
@@ -0,0 +1,38 @@
+package edu.kis.vh.nursery.list;
+
+public class IntArrayStack implements StackInterface {
+		private static final int FULL_STACK_VALUE = 11;
+		private static final int EMPTY_STACK_VALUE = -1;
+		private static final int STACK_CAPACITY = 12;
+		private int[] numbers = new int[STACK_CAPACITY];
+		private int total = EMPTY_STACK_VALUE;
+
+		public int getTotal() {
+			return total;
+		}
+
+		public void push(int in) {
+			if (!isFull())
+				numbers[++total] = in;
+		}
+
+		public boolean isEmpty() {
+			return total == EMPTY_STACK_VALUE;
+		}
+
+		public boolean isFull() {
+			return total == FULL_STACK_VALUE;
+		}
+
+		public int top() {
+			if (isEmpty())
+				return EMPTY_STACK_VALUE;
+			return numbers[total];
+		}
+
+		public int pop() {
+			if (isEmpty())
+				return EMPTY_STACK_VALUE;
+			return numbers[total--];
+		}
+}
