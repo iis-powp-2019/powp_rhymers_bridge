96ac6934ff131ad8e53b017a456c0082c742756f
skydriver95 <34252075+skydriver95@users.noreply.github.com>
2019-04-02 17:12:22 +0200

    Zad. 3.5 - zmieniono typ atrybutu na interfejs

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,24 +3,24 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.list.IntLinkedList;
 
 public class DefaultCountingOutRhymer {
-    private IntLinkedList intLinkedList;
+    private Stackable stackable;
 
     private final int COLLECTION_EMPTY_SIZE=-1;
 
     public DefaultCountingOutRhymer(IntLinkedList intLinkedList) {
-        this.intLinkedList = intLinkedList;
+        this.stackable = new IntLinkedList();
     }
 
     public DefaultCountingOutRhymer() {
-        this.intLinkedList = new IntLinkedList();
+        this.stackable = new IntLinkedList();
     }
 
     public void countIn(int in) {
-        intLinkedList.push(in);
+        stackable.push(in);
     }
 
     public boolean callCheck() {
-        return intLinkedList.isEmpty();
+        return stackable.isEmpty();
     }
 
     public boolean isFull() {
@@ -28,12 +28,12 @@ public class DefaultCountingOutRhymer {
     }
 
     public int peekaboo() {
-        if(intLinkedList.isEmpty())
+        if(stackable.isEmpty())
             return COLLECTION_EMPTY_SIZE;
-        return intLinkedList.top();
+        return stackable.top();
     }
 
     public int countOut() {
-        return intLinkedList.pop();
+        return stackable.pop();
     }
 }
