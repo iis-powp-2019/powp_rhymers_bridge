9d280c5581e7ef81245ccafd7c717a86da645ad8
Patryk Gitner <209317@edu.p.lodz.pl>
2019-04-02 18:12:08 +0200

    3.1.11 - zmiana atrybutu temp z komentarzem

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,23 +1,23 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stacks.LinkedListOfIntegers;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    // 3.1.11 - w tym przypadku lepsze będzie poprzednie rozwiązanie, zawierające klasę DefaultCountingOutRhymer z uwagi na to,
+    // że w tamtej klasie dochodzi do 'wyboru' implementacji naszego stosu.
+    private Stack stack = new LinkedListOfIntegers();
 
     @Override public int countOut() {
         while (!callCheck()) {
-            temp.countIn(super.countOut());
+            stack.countIn(super.countOut());
         }
 
-        int ret = temp.countOut();
-        while (!temp.callCheck()) {
-            countIn(temp.countOut());
+        int ret = stack.countOut();
+        while (!stack.callCheck()) {
+            countIn(stack.countOut());
         }
 
         return ret;
-    }
-
-    public DefaultCountingOutRhymer getTemp() {
-        return temp;
     }
 }
