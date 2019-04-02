c7e6df58ea42952998e1d7a787850780f52aac70
Rahly <karol.oleksy97@gmail.com>
2019-04-02 23:13:19 +0200

    3.1.10 Stworzenie konstruktorów

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,17 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+    public FIFORhymer(IntLinkedList intLinkedList) {
+        super(intLinkedList);
+    }
+
+    public FIFORhymer() {
+    }
 
     public int countOut() {
         while (!isEmpty())
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,8 +1,19 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
+
+    public HanoiRhymer(IntLinkedList intLinkedList, int totalRejected) {
+        super(intLinkedList);
+        this.totalRejected = totalRejected;
+    }
+
+    public HanoiRhymer(int totalRejected) {
+        this.totalRejected = totalRejected;
+    }
 
     public int reportRejected() {
         return totalRejected;
