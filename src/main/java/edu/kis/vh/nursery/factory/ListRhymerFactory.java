585d998d75dee79e43e9299df928ad2b182247fb
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-04-02 15:05:59 +0200

    3.1.12 Implementacja fabryk

---
+++ src/main/java/edu/kis/vh/nursery/factory/ListRhymerFactory.java
@@ -0,0 +1,33 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.list.datastructures.IntArrayStack;
+import edu.kis.vh.nursery.list.datastructures.IntLinkedList;
+
+public class ListRhymerFactory implements RhymersFactory {
+
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntLinkedList());
+
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+
+}
