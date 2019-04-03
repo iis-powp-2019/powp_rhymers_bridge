d599931bdd569306c6a4fc2b7bde99471eb031a6
Marcin Jeznach <scheissig@gmail.com>
2019-04-03 15:20:18 +0200

    Wykonano punkty 3.1.1 - 3.1.2 - delegacja implementacji stosu do oddzielnej klasy IntArrayStack

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,83 +5,37 @@ package edu.kis.vh.nursery;
  */
 public class DefaultCountingOutRhymer {
 
-    /**
-     * Value of {@link #total total} when rhymer stores no numbers
-     */
-    private static final int NUMBERS_EMPTY = -1;
-    /**
-     * Value returned by {@link #peekaboo() peekaboo()} or {@link #countOut() countOut()} when rhymer stores no number
-     */
-    private static final int NUMBERS_EMPTY_VALUE = -1;
-    /**
-     * Maximum quantity of numbers to store in rhymer
-     */
-    private static final int NUMBERS_MAX_SIZE = 12;
-    // TODO: needs refactoring to use list.IntLinkedList
-    /**
-     * Array of numbers stored in rhymer
-     */
-    private final int[] NUMBERS = new int[NUMBERS_MAX_SIZE];
+    IntArrayStack stack;
 
-    /**
-     * Getter to {@link #total total} field
-     * @return value of {@link #total total} field
-     */
+    public DefaultCountingOutRhymer() {
+        this.stack = new IntArrayStack();
+    }
+
+    public DefaultCountingOutRhymer(IntArrayStack stack) {
+        this.stack = stack;
+    }
+
     public int getTotal() {
-        return total;
+        return stack.getTotal();
     }
 
-    /**
-     * Quantity of numbers stored in rhymer
-     */
-    private int total = NUMBERS_EMPTY;
-
-    /**
-     * Adds number to rhymer
-     * @param IN value to add to the rhymer
-     */
-    public void countIn(final int IN) {
-
-        if (!isFull())
-            NUMBERS[++total] = IN;
+    public void countIn(int IN) {
+        stack.countIn(IN);
     }
 
-    /**
-     * Checks if rhymer number storage is empty
-     * @return true when rhymers stores no numbers
-     */
-    boolean callCheck() {
-        return total == NUMBERS_EMPTY;
+    public boolean callCheck() {
+        return stack.callCheck();
     }
 
-    /**
-     * Checks if rhymer number storage is full
-     * @return true when rhymers can store no more numbers
-     */
-    boolean isFull() {
-        return total == NUMBERS_MAX_SIZE-1;
+    public boolean isFull() {
+        return stack.isFull();
     }
 
-    /**
-     * Gets value of last added number
-     * @return last added number
-     */
-    int peekaboo() {
-
-        if (callCheck())
-            return NUMBERS_EMPTY_VALUE;
-        return NUMBERS[total];
+    public int peekaboo() {
+        return stack.peekaboo();
     }
 
-    /**
-     * Gets value of last added number, popping it from rhymer storage
-     * @return last added number
-     */
-    public int countOut () {
-
-        if (callCheck())
-            return NUMBERS_EMPTY_VALUE;
-        return NUMBERS[total--];
+    public int countOut() {
+        return stack.countOut();
     }
-
 }
