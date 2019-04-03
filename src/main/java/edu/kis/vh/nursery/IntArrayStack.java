809409588705794d9e59b015f78df080c664a41b
Your Name <mail@test.com>
2019-04-03 14:45:45 +0200

    Bridge 3.1.1 stworzenie IntArrayStack

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,54 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+	private static final int MAX_STACK_SIZE = 12;
+
+	private static final int RETURN_DEFAULT = -1;
+	
+	private static final int INITIAL_STACK_INDEX = -1;
+
+	private int[] numbers = new int[MAX_STACK_SIZE];
+
+	private int total = INITIAL_STACK_INDEX;
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void countIn(int in) {
+		if (!isFull())
+			numbers[++total] = in;
+	}
+
+	public boolean callCheck() {
+		return total == INITIAL_STACK_INDEX;
+	}
+
+	public boolean isFull() {
+		return total == MAX_STACK_SIZE - 1;
+	}
+
+	protected int peekaboo() {
+		if (callCheck())
+			return RETURN_DEFAULT;
+		return numbers[total];
+	}
+
+	public int countOut() {
+		if (callCheck())
+			return RETURN_DEFAULT;
+		return numbers[total--];
+	}
+
+	public static int getInitialStackIndex() {
+		return INITIAL_STACK_INDEX;
+	}
+
+	public static int getReturnDefault() {
+		return RETURN_DEFAULT;
+	}
+
+	public static int getMaxStackSize() {
+		return MAX_STACK_SIZE;
+	}
+}
