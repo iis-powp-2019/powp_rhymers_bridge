755e901d9852b2fd6d9009bc688c60839216f9c3
Kalinowski.Mikołaj <209347@edu.p.lodz.pl>
2019-04-03 20:51:03 +0200

    3.1.12 - Implemented IntArrayStack and IntLinkedList factories

---
+++ src/main/java/edu/kis/vh/nursery/factory/IntLinkedListRhymersFactory.java
@@ -0,0 +1,29 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FirstInFirstOutRhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
+public class IntLinkedListRhymersFactory implements RhymersFactory{
+    @Override
+    public DefaultCountingOutRhymer GetStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer GetFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer GetFIFORhymer() {
+        return new FirstInFirstOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer GetHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+}
