05e81936601f57ba9e7bc837fba5ea42a15d1d0c
Nabial <vxd55555@gmail.com>
2019-04-03 15:00:27 +0200

    Fixed import in DefaultRhymersFactory.

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery.factory;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
@@ -9,12 +9,12 @@ public class DefaultRhymersFactory implements Rhymersfactory {
 
 	@Override
 	public DefaultCountingOutRhymer GetStandardRhymer() {
-		return new defaultCountingOutRhymer();
+		return new DefaultCountingOutRhymer();
 	}
 
 	@Override
 	public DefaultCountingOutRhymer GetFalseRhymer() {
-		return new defaultCountingOutRhymer();
+		return new DefaultCountingOutRhymer();
 	}
 
 	@Override
