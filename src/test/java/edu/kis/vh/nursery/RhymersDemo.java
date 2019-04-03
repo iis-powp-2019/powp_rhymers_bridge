2f53e4f78efc4ca44869d5dfb71e9993909a2cb8
Rahly <karol.oleksy97@gmail.com>
2019-04-03 22:34:11 +0200

    3.1.13 DOdanie testów

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,6 +1,8 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.factory.ArrayRhymersFactory;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
+import edu.kis.vh.nursery.factory.ListRhymersFactory;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
@@ -8,7 +10,12 @@ class RhymersDemo {
     public static void main(String[] args) {
 
         RhymersFactory factory = new DefaultRhymersFactory();
+        RhymersFactory listFactory = new ListRhymersFactory();
+        RhymersFactory arrayFactory = new ArrayRhymersFactory();
+
         testRhymers(factory);
+        testRhymers(listFactory);
+        testRhymers(arrayFactory);
     }
 
     private static void testRhymers(RhymersFactory factory) {
