7660d542c09c60da0d64405ee5e476de12743693
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-04-03 04:15:43 +0200

    lab_2 package name changed (3.1.7)

---
+++ src/main/java/edu/kis/vh/nursery/dataStructures/IntLinkedList.java
@@ -0,0 +1,90 @@
+package edu.kis.vh.nursery.list;
+
+import edu.kis.vh.nursery.IStackAndListSimilarities;
+
+public class IntLinkedList implements IStackAndListSimilarities {
+
+    private Node last;
+    private int i;
+
+
+
+    @Override public void countIn(int i) {
+        if (getLast() == null)
+            setLast(new Node(i));
+        else {
+            getLast().setNext(new Node(i));
+            getLast().getNext().setPrev(getLast());
+            setLast(getLast().getNext());
+        }
+    }
+
+    @Override public boolean callCheck() {
+        return getLast() == null;
+    }
+
+    @Override  public boolean isFull() {
+        return false;
+    }
+
+    @Override public int peekaboo() {
+        if (callCheck())
+            return -1;
+        return getLast().getValue();
+    }
+
+    @Override public int countOut() {
+        if (callCheck())
+            return -1;
+        int ret = getLast().getValue();
+        setLast(getLast().getPrev());
+        return ret;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
+
+    public int getI() {
+        return i;
+    }
+    class Node {
+
+        private int value;
+        private Node prev;
+        private Node next;
+
+        Node(int i) {
+            setValue(i);
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+}
