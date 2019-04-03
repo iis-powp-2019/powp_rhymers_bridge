a1cfba6b56241b038353281ef333a0bacd4ff6c6
Matixz <matixz.kom@gmail.com>
2019-04-03 02:12:16 +0200

    3.1.7 refaktoryzacja nazwy

---
+++ src/main/java/edu/kis/vh/nursery/stack/Node.java
@@ -0,0 +1,19 @@
+package edu.kis.vh.nursery.list;
+
+public class Node {
+
+	protected int value;
+	public Node prev, next;
+
+	public Node(int i) {
+		setValue(i);
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
