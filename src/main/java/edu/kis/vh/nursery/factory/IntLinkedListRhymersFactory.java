3173df4a23035e6d80fae98d3d281bb874390e9c
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-04-03 21:45:09 +0200

    3.12. add IntArrayStackRhymersFactory and IntLinkedListRhymersFactory

---
+++ src/main/java/edu/kis/vh/nursery/factory/IntLinkedListRhymersFactory.java
@@ -0,0 +1,28 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.datastructures.IntLinkedList;
+
+public class IntLinkedListRhymersFactory implements RhymersFactory {
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+}
