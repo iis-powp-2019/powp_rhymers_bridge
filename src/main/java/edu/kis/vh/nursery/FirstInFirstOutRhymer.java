d2e2ac2ad5cb0ba6fd2f39f681382e6ad99183ec
Kalinowski.Mikołaj <209347@edu.p.lodz.pl>
2019-04-03 20:36:02 +0200

    3.1.11 - Changed temp attribute to IntArrayStack

--- src/main/java/edu/kis/vh/nursery/FirstInFirstOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FirstInFirstOutRhymer.java
@@ -1,10 +1,12 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
 import edu.kis.vh.nursery.stack.Stackable;
 
 public class FirstInFirstOutRhymer extends DefaultCountingOutRhymer {
 
-	private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+	private final Stackable temp = new IntArrayStack();
 
 	public FirstInFirstOutRhymer(Stackable stack) {
 		super(stack);
@@ -27,3 +29,5 @@ public class FirstInFirstOutRhymer extends DefaultCountingOutRhymer {
 	}
 }
 //Kombinacja ctrl+alt+ strzałka w lewo sprawia, że przechodzimy wstecz po histori aktualnie otwartych kart, z kolei ctrl + alt + strzałka w prawo idzie do przodu w kolejości otwartych kart.
+
+//IntArrayStack jest szybszy od IntLinkedList.
