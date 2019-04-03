9368190ca9a0f75ca261826adfb97525631753c6
wsobczyk <wiktoriasobczyk@gmail.com>
2019-04-03 14:47:18 +0200

    3.1.1 copy stack implementation from DefaultCountingOutRyhmer to IntArrayStack class

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,60 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+    private static final int DEFAULT_INDEX_STACK = -1;
+
+    private static final int NOT_FOUND_RETURN = -1;
+
+    private static final int MAX_SIZE_STACK = 12;
+
+    private final int[] numbers = new int[getMaxSizeStack()];
+
+    private int total = getDefaultIndexStack();
+
+    private static int getDefaultIndexStack() {
+        return DEFAULT_INDEX_STACK;
+    }
+
+    private static int getNotFoundReturn() {
+        return NOT_FOUND_RETURN;
+    }
+
+    private static int getMaxSizeStack() {
+        return MAX_SIZE_STACK;
+    }
+
+    protected void countIn(int in) {
+        if (!isFull())
+            getNumbers()[++total] = in;
+    }
+
+    protected boolean callCheck() {
+        return total == getDefaultIndexStack();
+    }
+
+    protected boolean isFull() {
+        return total == getMaxSizeStack() -1;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return getNotFoundReturn();
+        return getNumbers()[total];
+    }
+
+    protected int countOut() {
+        if (callCheck())
+            return getNotFoundReturn();
+        return getNumbers()[total--];
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    private int[] getNumbers() {
+        return numbers;
+    }
+
+
+}
