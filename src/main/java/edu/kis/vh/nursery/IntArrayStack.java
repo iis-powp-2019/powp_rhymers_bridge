c8aa7e4f99ebfdbb604c63a5b35cc9094ffb2b13
Daniel Kowalczyk <209364@edu.p.lodz.pl>
2019-04-04 08:49:28 +0200

    Skopiowanie stosu z DefaultCountingOutRyhmer do IntArrayStack

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,40 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+    private static final int MAXSIZE = 12;
+    private static final int DEFAULT_STACK_VALUE = -1;
+    private static final int MAX_STACK_VALUE = 11;
+    private static final int ERROR_NUMBER = -1;
+    private final int[] numbers = new int[MAXSIZE];
+
+    private int total = DEFAULT_STACK_VALUE;
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == DEFAULT_STACK_VALUE;
+    }
+
+    public boolean isFull() {
+        return total == MAX_STACK_VALUE;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return ERROR_NUMBER;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return ERROR_NUMBER;
+        return numbers[total--];
+    }
+
+    public int getTotal() {
+        return total;
+    }
+}
