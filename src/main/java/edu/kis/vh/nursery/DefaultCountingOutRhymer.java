a5a4370e2b09e93bbf1e5776c0db904b47a89619
209450 <209450@edu.p.lodz.pl>
2019-04-04 10:58:17 +0200

    Generalizacja typów na interfejsu IntLinkedListInterface

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,40 +1,41 @@
 package edu.kis.vh.nursery;
 
 import edu.kis.vh.nursery.list.IntLinkedList;
+import edu.kis.vh.nursery.list.IntLinkedListInterface;
 
 public class DefaultCountingOutRhymer {
-    IntLinkedList intLinkedList;
+    IntLinkedListInterface intLinkedListInterface;
 
     public DefaultCountingOutRhymer() {
-        intLinkedList = new IntLinkedList();
+        intLinkedListInterface= new IntLinkedList();
     }
 
     public DefaultCountingOutRhymer(IntLinkedList intLinkedList) {
-        this.intLinkedList = intLinkedList;
+        this.intLinkedListInterface = intLinkedList;
     }
 
     public void countIn(int in) {
-        intLinkedList.push(in);
+        intLinkedListInterface.push(in);
     }
 
     public boolean callCheck() {
-        return intLinkedList.isEmpty();
+        return intLinkedListInterface.isEmpty();
     }
 
     public boolean isFull() {
-        return intLinkedList.isFull();
+        return intLinkedListInterface.isFull();
     }
 
     public int peekaboo() {
-        return intLinkedList.top();
+        return intLinkedListInterface.top();
     }
 
     public int countOut() {
-        return intLinkedList.pop();
+        return intLinkedListInterface.pop();
     }
 
     public int getIndexOutOfArray(){
-        if(intLinkedList.isEmpty())
+        if(intLinkedListInterface.isEmpty())
             return -1;
         else
             return 0;
