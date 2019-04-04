f59cdb3c95f782f45f5c0ee2a6e3b338817c9a9a
Daniel Kowalczyk <209364@edu.p.lodz.pl>
2019-04-04 10:24:43 +0200

    Dodanie konstruktorów do HanoiRhymer i FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,19 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.container.IntLinkedList;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+    public FIFORhymer(IntLinkedList list, DefaultCountingOutRhymer temp) {
+        super(list);
+        this.temp = temp;
+    }
+
+    public FIFORhymer(DefaultCountingOutRhymer temp) {
+        this.temp = temp;
+    }
 
     @Override
     public int countOut() {
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,8 +1,19 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.container.IntLinkedList;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
+
+    public HanoiRhymer(IntLinkedList list, int totalRejected) {
+        super(list);
+        this.totalRejected = totalRejected;
+    }
+
+    public HanoiRhymer(int totalRejected) {
+        this.totalRejected = totalRejected;
+    }
 
     public int reportRejected() {
         return totalRejected;
