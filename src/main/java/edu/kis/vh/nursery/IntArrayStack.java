075039963ae384770842bfe626c39b4c76812074
Leherguargin <Leherguargin@users.noreply.github.com>
2019-04-05 12:02:32 +0200

    lab2 3.1.1 refaktoryzacja do wzorca Bridge

---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,36 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    private static final int MINUS_JEDEN = -1;
+    private static final int DWANASCIE = 12;
+    private int[] NUMBERS = new int[DWANASCIE];
+
+
+    private int total = MINUS_JEDEN;
+
+    public void countIn(int in) {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == MINUS_JEDEN;
+    }
+
+    public boolean isFull() { //formatowanie kodu: za duze wciecie.
+        return total == 11;
+    }
+
+    protected int peekaboo() { //formatowanie kodu: za duze wciecie x2
+        if (callCheck())
+            return MINUS_JEDEN;
+        return NUMBERS[total];
+    }
+
+    public int countOut() { //formatowanie kodu: za duze wcięcie x3
+        if (callCheck())
+            return MINUS_JEDEN;
+        return NUMBERS[total--];
+    }
+}
