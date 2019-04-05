a966c9b7d4b12ca79deba428981a70b0201c0e46
Leherguargin <Leherguargin@users.noreply.github.com>
2019-04-05 14:01:04 +0200

    lab2 3.1.12 + poprawa testów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,6 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.collections.IntArrayStack;
 import edu.kis.vh.nursery.collections.IntLinkedList;
 import edu.kis.vh.nursery.collections.Stack;
 
@@ -8,6 +9,10 @@ public class DefaultCountingOutRhymer {
 	Stack intArrayStack = new IntLinkedList();
 
 	public DefaultCountingOutRhymer(IntLinkedList intArrayStack) {
+		this.intArrayStack = intArrayStack;
+	}
+
+	public DefaultCountingOutRhymer(IntArrayStack intArrayStack) {
 		this.intArrayStack = intArrayStack;
 	}
 
