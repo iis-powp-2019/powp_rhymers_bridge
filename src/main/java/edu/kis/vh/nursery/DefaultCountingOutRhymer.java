5939efafb0c56e69df7355a9048378d7b8aceace
209515 <209515@edu.p.lodz.pl>
2019-04-09 21:13:12 +0200

    Lab 1. Zad 3.3.2 Dodanie dokumentacji do klasy DefaultCountingOutRhymes

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,35 +2,66 @@ package edu.kis.vh.nursery;
 
 import edu.kis.vh.nursery.list.IntLinkedList;
 
+/**
+ * @author 209515
+ * Klasa wyliczaki opartej o listę
+ */
 public class DefaultCountingOutRhymer {
 
 	private IntLinkedList list;
 
+	/**
+	 * @param in
+	 * Metoda dodaje element in do listy
+	 */
 	public void countIn(final int in) {
 		list.push(in);
 	}
 
+	/**
+	 * @return
+	 * Metoda sprawdza czy lista jest pusta
+	 */
 	public boolean callCheck() {
 		return list.isEmpty();
 	}
 
+	/**
+	 * @return
+	 * Metoda sprawdza czy lista jest pełna
+	 */
 	public boolean isFull() {
 		return list.isFull();
 	}
 
+	/**
+	 * @return
+	 * Metoda zwraca ostatni element listy
+	 */
 	public int countOut() {
 		return list.pop();
 	}
 
+	/**
+	 * Domyslny konstruktor klasy
+	 */
 	public DefaultCountingOutRhymer() {
 		list = new IntLinkedList();
 	}
 	
+	/**
+	 * @param list
+	 * Konstruktor inicjalizujacy pole list
+	 */
 	public DefaultCountingOutRhymer(final IntLinkedList list) {
 		super();
 		this.list = list;
 	}
 
+	/**
+	 * @return
+	 * Zwraca wartosc ostatniego elementu
+	 */
 	public int peekaboo() {
 		return list.top();
 	}
