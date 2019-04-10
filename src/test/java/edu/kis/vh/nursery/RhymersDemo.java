cb18462e1eec79a8576796d979fe8a727b7c84b5
wsobczyk <wiktoriasobczyk@gmail.com>
2019-04-10 21:42:17 +0200

    3.1.13 added test to RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,22 +1,29 @@
 package edu.kis.vh.nursery;
 
-import edu.kis.vh.nursery.DefaultCountingOutRhymer;
-import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
+import edu.kis.vh.nursery.factory.ArrayRhymersFactory;
+import edu.kis.vh.nursery.factory.ListRhymersFactory;
+
+;
 
 class RhymersDemo {
 
-
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        testRhymers(factory);
+        DefaultRhymersFactory defaultRhymersFactory = new DefaultRhymersFactory();
+        ArrayRhymersFactory arrayRhymersFactory = new ArrayRhymersFactory();
+        ListRhymersFactory listRhymersFactory = new ListRhymersFactory();
 
+        testRhymers(factory);
+        testRhymers(defaultRhymersFactory);
+        testRhymers(arrayRhymersFactory);
+        testRhymers(listRhymersFactory);
     }
 
     private static void testRhymers(Rhymersfactory factory) {
-        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
-                factory.getFIFORhymer(), factory.getHanoiRhymer()};
+        DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(), factory.getFIFORhymer(),
+                factory.getHanoiRhymer()};
 
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
