8bf949d485757ab0ede27ecb5331213cf39b2fab
ostrowski.kamil <209418@edu.p.lodz.pl>
2019-04-10 18:58:21 +0200

    Wykonanie punktu 3.1.12. Created Factories for Rhymers.

---
+++ src/main/java/edu/kis/vh/nursery/factory/IntLinkedListRhymersFactory.java
@@ -0,0 +1,29 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.structures.IntArrayStack;
+import edu.kis.vh.nursery.structures.IntLinkedList;
+
+public class IntLinkedListRhymersFactory implements Rhymersfactory {
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+}
