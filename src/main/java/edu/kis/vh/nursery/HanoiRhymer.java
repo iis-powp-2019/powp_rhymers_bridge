de8babf6eecc25e6be85a148044132a6702dcd80
wsobczyk <wiktoriasobczyk@gmail.com>
2019-04-10 21:05:52 +0200

    3.1.10 generate constructors from superclass

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,8 +1,19 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.storage.IntStorageInterface;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
+
+    public HanoiRhymer(int totalRejected) {
+        this.totalRejected = totalRejected;
+    }
+
+    public HanoiRhymer(IntStorageInterface intStorageInterface, int totalRejected) {
+        super(intStorageInterface);
+        this.totalRejected = totalRejected;
+    }
 
     protected int reportRejected() {
         return getTotalRejected();
