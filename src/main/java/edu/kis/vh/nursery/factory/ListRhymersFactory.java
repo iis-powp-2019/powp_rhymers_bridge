7d91dd551c37b705aba98a58c031b7d3a0e38719
Jakub <kuba.jakub17@gmail.com>
2019-04-14 21:41:24 +0200

    Ppunk 12

---
+++ src/main/java/edu/kis/vh/nursery/factory/ListRhymersFactory.java
@@ -0,0 +1,33 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+
+public class ListRhymersFactory
+	implements RhymersFactory
+{
+	@Override
+	public DefaultCountingOutRhymer GetStandardRhymer()
+	{
+		return new HanoiRhymer();
+	}
+	
+	@Override
+	public DefaultCountingOutRhymer GetFalseRhymer()
+	{
+		return new FIFORhymer();
+	}
+	
+	@Override
+	public DefaultCountingOutRhymer GetFIFORhymer()
+	{
+		return new FIFORhymer();
+	}
+	
+	@Override
+	public DefaultCountingOutRhymer GetHanoiRhymer()
+	{
+		return new HanoiRhymer();
+	}
+}
