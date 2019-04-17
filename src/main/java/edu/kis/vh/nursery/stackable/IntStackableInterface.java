a97505a091f186c0d4c162659cab3e09cef8ff4d
Piotr Rogowski <polishotaku@gmail.com>
2019-04-17 22:33:43 +0200

    3.1.9 - Changed value returned by methods in case of empty stack

--- src/main/java/edu/kis/vh/nursery/stackable/IntStackableInterface.java
+++ src/main/java/edu/kis/vh/nursery/stackable/IntStackableInterface.java
@@ -1,7 +1,7 @@
 package edu.kis.vh.nursery.stackable;
 
 public interface IntStackableInterface {
-    static final int RETURN_VALUE = -1;
+    static final int RETURN_VALUE = 0;
     static final int INITIAL_STACK_INDEX = -1;
 
     void countIn(int i);
