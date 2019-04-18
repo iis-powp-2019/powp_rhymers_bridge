96e73bada6a470399e11d41c09c26332a1773217
Piotr Rogowski <polishotaku@gmail.com>
2019-04-18 11:58:06 +0200

    3.1.12 - Added factories to ArrayStack and to LinkedList.

---
+++ src/main/java/edu/kis/vh/nursery/factory/ListRhymersFactory.java
@@ -0,0 +1,30 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.stackable.IntArrayStack;
+import edu.kis.vh.nursery.stackable.IntLinkedList;
+
+public class ListRhymersFactory implements RhymersFactory{
+
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+}
