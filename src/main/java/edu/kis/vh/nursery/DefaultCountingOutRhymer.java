3daa2fd4f6a00ad1c3ee719f4779c23f44f45e6b
Delicja05 <dewicusia@op.pl>
2019-04-19 19:18:18 +0200

    3.1.3. - cd

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,16 +4,10 @@ import edu.kis.vh.nursery.list.IntLinkedList;
 
 public class DefaultCountingOutRhymer {
 
-//	IntArrayStack intArrayStack = new IntArrayStack();
 	IntLinkedList intLinkedList = new IntLinkedList();
 	
 	public DefaultCountingOutRhymer() {	
 	}
-	
-//	public DefaultCountingOutRhymer(IntArrayStack intArrayStack) {
-//		super();
-//		this.intArrayStack = intArrayStack;
-//	}
 	
 	public DefaultCountingOutRhymer(IntLinkedList intLinkedList) {
 		super();
@@ -21,32 +15,26 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public void countIn(int in) {
-//		intArrayStack.countIn(in);
 		intLinkedList.push(in);
 	}
 
 	public boolean callCheck() {
-//		return intArrayStack.callCheck();
 		return intLinkedList.isEmpty();
 	}
 
 	public boolean isFull() {
-//		return intArrayStack.isFull();
 		return intLinkedList.isFull();
 	}
 
 	public int peekaboo() {
-//		return intArrayStack.peekaboo();
 		return intLinkedList.top();
 	}
 
 	public int countOut() {
-//		return intArrayStack.countOut();
 		return intLinkedList.pop();
 	}
 
 	public int getTotal() {
-//		return intArrayStack.getTotal();
 		return intLinkedList.getTotal();
 	}
 
