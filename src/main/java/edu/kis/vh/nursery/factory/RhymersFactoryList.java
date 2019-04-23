c6245d9bf3e42ee0549b44d38a0fedb78935ca76
pallineczka <paulina.stelmasiak97@gmail.com>
2019-04-24 01:16:30 +0200

    3.1.12 implementacja dwóch fabryk implementujących RhymersFactory

---
+++ src/main/java/edu/kis/vh/nursery/factory/RhymersFactoryList.java
@@ -0,0 +1,29 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
+import edu.kis.vh.nursery.FIFORhymer;
+import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.stack.IntArrayStack;
+import edu.kis.vh.nursery.stack.IntLinkedList;
+
+public class RhymersFactoryList implements Rhymersfactory{
+    @Override
+    public DefaultCountingOutRhymer getStandardRhymer() {
+        return new DefaultCountingOutRhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFalseRhymer() {
+        return new DefaultCountingOutRhymer(new IntArrayStack());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getFIFORhymer() {
+        return new FIFORhymer(new IntLinkedList());
+    }
+
+    @Override
+    public DefaultCountingOutRhymer getHanoiRhymer() {
+        return new HanoiRhymer(new IntLinkedList());
+    }
+}
