26754e5bf0cdd920573bea695bfb308786ce44d8
Mikołaj Baran <tarralan@wp.pl>
2019-05-26 14:10:42 +0200

    3.1.9 Zmiana warotoci zrwacanej dla pustej kolekcji z -1 na 0

--- src/main/java/edu/kis/vh/nursery/collections/IntCollection.java
+++ src/main/java/edu/kis/vh/nursery/collections/IntCollection.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.collections;
 
 public interface IntCollection {
     int STACK_STARTING_POSITION = -1;
-    int STACK_VALUE_ON_STARTING_POSITION = -1;
+    int STACK_VALUE_ON_STARTING_POSITION = 0;
     void push(int x);
     boolean isEmpty();
     boolean isFull();
