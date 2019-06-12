a435bb7be23c40a8a7c6c853a0a88bca1b293ed9
Tobiasz Skorża <209458@edu.p.lodz.pl>
2019-06-12 14:01:26 +0200

    Lab 2 3.1.13

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -2,18 +2,25 @@ package edu.kis.vh.nursery;
 
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
+import edu.kis.vh.nursery.factory.ArrayStackRhymersFactory;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
+import edu.kis.vh.nursery.factory.ListStackRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
 	public static void main(String[] args) {
 		Rhymersfactory factory = new DefaultRhymersFactory();
+		Rhymersfactory arrayFactory = new ArrayStackRhymersFactory();
+		Rhymersfactory listFactory = new ListStackRhymersFactory();
+
+		testRhymers(arrayFactory);
+		testRhymers(listFactory);
 		testRhymers(factory);
 	}
 		private static void testRhymers(Rhymersfactory factory) {
-			DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
-					factory.GetFIFORhymer(), factory.GetHanoiRhymer() };
+			DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
+					factory.getFIFORhymer(), factory.getHanoiRhymer() };
 	
 			for (int i = 1; i < 15; i++)
 				for (int j = 0; j < 3; j++)
