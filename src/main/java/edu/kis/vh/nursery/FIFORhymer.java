c29f33bb756ad94b8ed7bba9b2aa151c569c4ad4
hadi97 <hadielwazer97@gmail.com>
2019-06-24 10:49:45 +0200

    adding constructors to supclasses

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,15 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stackModules.Data;
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private DefaultCountingOutRhymer temp;
+
+    public FIFORhymer(Data data, DefaultCountingOutRhymer temp) {
+        super(data);
+        this.temp = temp;
+    }
 
     @Override
     public int countOut() {
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,9 +1,15 @@
 package edu.kis.vh.nursery;
 
+import edu.kis.vh.nursery.stackModules.Data;
+
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private static final int ZERO = 0;
     private int totalRejected = ZERO;
+
+    public HanoiRhymer(Data data) {
+        super(data);
+    }
 
     public int reportRejected() {
         return totalRejected;
